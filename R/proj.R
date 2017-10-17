d0efe98b707b04e284a98cb02b5e83bb292b16be
hadley <h.wickham@gmail.com>
2017-10-17 12:34:49 -0500

    Fix failing test

--- R/proj.R
+++ R/proj.R
@@ -51,7 +51,7 @@ proj_set <- function(path = ".", force = FALSE) {
 }
 
 scoped_temporary_package <- function(dir = tempfile(), env = parent.frame()) {
-  old <- proj_get()
+  old <- proj$cur
   withr::defer(proj_set(old), envir = env)
 
   utils::capture.output(create_package(dir, rstudio = FALSE, open = FALSE))
