7197c947030f3a85c85a321a2af99f474e59a79e
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-20 17:10:38 -0800

    Silence tests

--- tests/testthat/test-use_readme.R
+++ tests/testthat/test-use_readme.R
@@ -1,24 +1,20 @@
 context("readme")
 
 test_that("error if try to overwrite existing file", {
-  tmp <- tempfile()
-  create_package(tmp, rstudio = FALSE)
+  tmp <- scoped_temporary_package()
   file.create(file.path(tmp, "README.md"))
-  expect_error(use_readme_md(tmp),
-               "already exists")
+  expect_error(use_readme_md(tmp), "already exists")
   file.create(file.path(tmp, "README.Rmd"))
-  expect_error(use_readme_rmd(tmp),
-               "already exists")
+  expect_error(use_readme_rmd(tmp), "already exists")
 })
 
 test_that("sets up git pre-commit hook iff pkg uses git", {
   # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
   skip_if(getRversion() < 3.2)
-  tmp <- tempfile()
-  create_package(tmp, rstudio = FALSE)
-  use_readme_rmd(open = FALSE)
+  tmp <- scoped_temporary_package()
+  capture_output(use_readme_rmd(open = FALSE))
   expect_false(file.exists(file.path(tmp, ".git", "hooks", "pre-commit")))
-  use_git()
-  use_readme_rmd(open = FALSE)
+  capture_output(use_git())
+  capture_output(use_readme_rmd(open = FALSE))
   expect_true(file.exists(file.path(tmp, ".git", "hooks", "pre-commit")))
 })
