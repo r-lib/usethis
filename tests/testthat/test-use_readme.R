3212c053c7a0455cb3a1b0411c48790970d06cad
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-22 12:15:52 -0800

    Restore test that fails on AppVeyor

--- tests/testthat/test-use_readme.R
+++ tests/testthat/test-use_readme.R
@@ -11,8 +11,6 @@ test_that("error if try to overwrite existing file", {
 test_that("sets up git pre-commit hook iff pkg uses git", {
   # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
   skip_if(getRversion() < 3.2)
-  # temporary fix!
-  skip_on_appveyor()
   tmp <- scoped_temporary_package()
   capture_output(use_readme_rmd(open = FALSE))
   expect_false(file.exists(file.path(tmp, ".git", "hooks", "pre-commit")))
