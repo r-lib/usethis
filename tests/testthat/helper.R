7839b6db32e9cc57ef936f0f5731155ad228dcd8
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-21 17:02:16 -0800

    Make scoped_temporary_package callable from the console
    Fixes #141

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,15 @@
 scoped_temporary_package <- function(dir = tempfile(), env = parent.frame()) {
   old <- proj$cur
-  withr::defer(proj_set(old), envir = env)
+  # Can't schedule a deferred project reset if calling this from the R console,
+  # which is useful when developing tests
+  if (identical(env, globalenv())) {
+    todo(
+      "Switching to a temporary project! To restore current project:\n",
+      "proj_set(\"", old, "\")"
+    )
+  } else {
+    withr::defer(proj_set(old), envir = env)
+  }
 
   utils::capture.output(create_package(dir, rstudio = FALSE, open = FALSE))
   invisible(dir)
