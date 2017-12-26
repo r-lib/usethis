edbdb025f539624e5b620968036cbd2c0a712c51
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-25 22:53:48 -0800

    Add scoped_temporary_project()

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,20 @@
 scoped_temporary_package <- function(dir = tempfile(),
                                      env = parent.frame(),
                                      rstudio = FALSE) {
+  scoped_temporary_thing(dir, env, rstudio, "package")
+}
+
+scoped_temporary_project <- function(dir = tempfile(),
+                                     env = parent.frame(),
+                                     rstudio = FALSE) {
+  scoped_temporary_thing(dir, env, rstudio, "project")
+}
+
+scoped_temporary_thing <- function(dir = tempfile(),
+                                   env = parent.frame(),
+                                   rstudio = FALSE,
+                                   thing = c("package", "project")) {
+  thing <- match.arg(thing)
   old <- proj$cur
   # Can't schedule a deferred project reset if calling this from the R console,
   # which is useful when developing tests
@@ -13,7 +27,11 @@ scoped_temporary_package <- function(dir = tempfile(),
     withr::defer(proj_set(old), envir = env)
   }
 
-  capture_output(create_package(dir, rstudio = rstudio, open = FALSE))
+  switch(
+    thing,
+    package = capture_output(create_package(dir, rstudio = rstudio, open = FALSE)),
+    project = capture_output(create_project(dir, rstudio = rstudio, open = FALSE))
+  )
   invisible(dir)
 }
 
