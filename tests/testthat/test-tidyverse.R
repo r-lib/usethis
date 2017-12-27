e057f381403a1a65a12e03e59e2b6521d4313fbf
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-26 22:24:28 -0800

    Add tests for tidyverse functions, where possible

---
+++ tests/testthat/test-tidyverse.R
@@ -0,0 +1,36 @@
+context("tidyverse")
+
+test_that("use_tidy_description() alphabetises dependencies", {
+  pkg <- scoped_temporary_package()
+  capture_output(use_package("usethis"))
+  capture_output(use_package("desc"))
+  capture_output(use_package("withr", "Suggests"))
+  capture_output(use_package("gh", "Suggests"))
+  capture_output(use_tidy_description())
+  desc <- readLines(file.path(proj_get(), "DESCRIPTION"))
+  expect_gt(grep("usethis", desc), grep("desc", desc))
+  expect_gt(grep("withr", desc), grep("gh", desc))
+})
+
+test_that("use_tidy_versions() specifies a version for all dependencies", {
+  pkg <- scoped_temporary_package()
+  capture_output(use_package("usethis"))
+  capture_output(use_package("desc"))
+  capture_output(use_package("withr", "Suggests"))
+  capture_output(use_package("gh", "Suggests"))
+  capture_output(use_tidy_versions())
+  desc <- readLines(file.path(proj_get(), "DESCRIPTION"))
+  desc <- grep("usethis|desc|withr|gh", desc, value = TRUE)
+  expect_true(all(grepl("\\(>= [0-9.]+\\)", desc)))
+})
+
+test_that("use_tidy_eval() inserts the template file and Imports rlang", {
+  pkg <- scoped_temporary_package()
+  ## fake the use of roxygen; this better in a test than use_roxygen_md()
+  capture_output(
+    use_description_field(name = "RoxygenNote", value = "6.0.1.9000")
+  )
+  capture_output(use_tidy_eval())
+  expect_match(list.files(file.path(proj_get(), "R")), "utils-tidy-eval.R")
+  expect_match(desc::desc_get("Imports", pkg), "rlang")
+})
