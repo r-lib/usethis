918d9a9c3800378f020b322a088aebbc5da313ca
Mara Averick <maraaverick@gmail.com>
2017-12-28 11:39:31 -0500

    Change CONDUCT template to use CODE_OF_CONDUCT * Naming convention used by GH Community Guidelines

--- R/code-of-conduct.R
+++ R/code-of-conduct.R
@@ -9,13 +9,13 @@
 #' @inheritParams use_template
 use_code_of_conduct <- function() {
   use_template(
-    "CONDUCT.md",
+    "CODE_OF_CONDUCT.md",
     ignore = TRUE
   )
 
   todo("Don't forget to describe the code of conduct in your README.md:")
   code_block(
-    "Please note that this project is released with a [Contributor Code of Conduct](CONDUCT.md).",
+    "Please note that this project is released with a [Contributor Code of Conduct](CODE_OF_CONDUCT.md).",
     "By participating in this project you agree to abide by its terms."
   )
 }
