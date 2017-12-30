36d817c2e54fabb678ca7e6d49b150f67ede9c27
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-12-30 09:17:19 -0800

    Early return

--- R/proj.R
+++ R/proj.R
@@ -26,20 +26,21 @@ is_package <- function(base_path = proj_get()) {
 }
 
 check_is_package <- function(whos_asking = NULL) {
-  if (!is_package()) {
-    message <- paste0(
-      "Project ", value(project_name()), " is not an R package."
-    )
-    if (!is.null(whos_asking)) {
-      message <- paste0(
-        code(whos_asking),
-        " is designed to work with packages. ",
-        message
-      )
-    }
-    stop(message, call. = FALSE)
+  if (is_package()) {
+    return(invisible())
   }
-  invisible()
+
+  message <- paste0(
+    "Project ", value(project_name()), " is not an R package."
+  )
+  if (!is.null(whos_asking)) {
+    message <- paste0(
+      code(whos_asking),
+      " is designed to work with packages. ",
+      message
+    )
+  }
+  stop(message, call. = FALSE)
 }
 
 #' Get and set currently active project
