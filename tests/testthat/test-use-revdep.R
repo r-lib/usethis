f4cd7ca8e93f65eac6e3a98f65b7d6d9bd1fce99
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-03 14:44:20 -0800

    Tweaks and tests for use_revdep()

---
+++ tests/testthat/test-use-revdep.R
@@ -0,0 +1,14 @@
+context("use_revdep")
+
+test_that("use_revdep() requires a package", {
+  scoped_temporary_project()
+  expect_error(use_revdep(), "not an R package")
+})
+
+test_that("use_revdep() creates and ignores files/dirs", {
+  scoped_temporary_package()
+  capture_output(use_revdep())
+  expect_true(file.exists(proj_path("revdep/email.yml")))
+  expect_true(file.exists(proj_path("revdep/.gitignore")))
+  expect_true(is_build_ignored("^revdep$"))
+})
