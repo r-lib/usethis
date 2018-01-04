bd88c65e7bf3f34a0b0f970e13e829e4ccca0346
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-03 20:46:28 -0800

    Style, document, test use_cran_comments()

---
+++ tests/testthat/test-use-cran-comments.R
@@ -0,0 +1,13 @@
+context("use-cran-comments")
+
+test_that("use_cran_comments() requires a package", {
+  scoped_temporary_project()
+  expect_error(use_cran_comments(), "not an R package")
+})
+
+test_that("use_cran_comments() creates and ignores the promised file", {
+  scoped_temporary_package()
+  capture_output(use_cran_comments())
+  expect_true(file.exists(proj_path("cran-comments.md")))
+  expect_true(is_build_ignored("^cran-comments\\.md$"))
+})
