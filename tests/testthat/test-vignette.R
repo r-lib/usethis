a691f7c6c3494eaade2b3827d54e1fdfb578e3ee
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-03 19:09:05 -0800

    Document, style, and test use_vignette()

---
+++ tests/testthat/test-vignette.R
@@ -0,0 +1,29 @@
+context("use-vignette")
+
+test_that("use_vignette() requires a package", {
+  scoped_temporary_project()
+  expect_error(use_vignette(), "not an R package")
+})
+
+test_that("use_vignette() requires a `name`", {
+  scoped_temporary_package()
+  expect_error(use_vignette(), "no default")
+})
+
+test_that("use_vignette() does the promised setup", {
+  scoped_temporary_package()
+  capture_output(use_vignette("name"))
+
+  ignores <- readLines(proj_path(".gitignore"), warn = FALSE)
+  expect_true("inst/doc" %in% ignores)
+
+  deps <- desc::desc_get_deps(proj_get())
+  expect_true(
+    all(c("knitr", "rmarkdown") %in% deps$package[deps$type == "Suggests"])
+  )
+
+  expect_identical(
+    desc::desc_get_or_fail("VignetteBuilder", proj_get()),
+    c(VignetteBuilder = "knitr")
+  )
+})
