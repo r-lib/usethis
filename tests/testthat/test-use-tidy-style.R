40255331bcd36ef28e7137d16e4842d502c2a016
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-08 20:58:22 +0100

    switch to usethis:::proj_path()

--- tests/testthat/test-use-tidy-style.R
+++ tests/testthat/test-use-tidy-style.R
@@ -3,7 +3,7 @@ context("use_tidy_style")
 test_that("styling the package works", {
   pkg <- scoped_temporary_package()
   capture_output(use_r("bad_style"))
-  path_to_bad_style <- file.path(pkg, "R/bad_style.R")
+  path_to_bad_style <- proj_path("R/bad_style.R")
   write_utf8(path_to_bad_style, "a=2\n")
   capture_output(use_tidy_style())
   expect_identical(readLines(path_to_bad_style), "a <- 2")
