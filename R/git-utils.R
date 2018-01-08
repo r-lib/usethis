e1462ee9232716295716d6970a024f8608a4e763
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-08 20:40:57 +0100

    add check_uncommitted_changed

--- R/git-utils.R
+++ R/git-utils.R
@@ -3,3 +3,12 @@ git_uncommitted <- function(path = ".") {
   st <- vapply(git2r::status(r), length, integer(1))
   any(st != 0)
 }
+
+check_uncommitted_changes <- function(path = proj_get()) {
+  if (uses_git(path) && git_uncommitted(path)) {
+    stop(
+      "Uncommited changes. Please commit to git before continuing",
+      call. = FALSE
+    )
+  }
+}
