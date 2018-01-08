d477e7e7e9a35ec2a1191c38257afb62a4beef70
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-08 20:52:45 +0100

    avoid EOF warning

--- tests/testthat/test-use-tidy-style.R
+++ tests/testthat/test-use-tidy-style.R
@@ -4,7 +4,7 @@ test_that("styling the package works", {
   pkg <- scoped_temporary_package()
   capture_output(use_r("bad_style"))
   path_to_bad_style <- file.path(pkg, "R/bad_style.R")
-  write_utf8(path_to_bad_style, "a=2")
+  write_utf8(path_to_bad_style, "a=2\n")
   capture_output(use_tidy_style())
   expect_identical(readLines(path_to_bad_style), "a <- 2")
   unlink(path_to_bad_style)
