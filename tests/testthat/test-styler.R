90615e42dd33e732d5a4aa443ba44c3dd0e14d0d
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-08 19:59:22 +0100

    add a test

---
+++ tests/testthat/test-styler.R
@@ -0,0 +1,11 @@
+context("use_tidy_style")
+
+test_that("styling the package works", {
+  pkg <- scoped_temporary_package()
+  capture_output(use_r("bad_style"))
+  path_to_bad_style <- file.path(pkg, "R/bad_style.R")
+  write_utf8(path_to_bad_style, "a=2")
+  capture_output(use_tidy_style())
+  expect_identical(readLines(path_to_bad_style), "a <- 2")
+  unlink(path_to_bad_style)
+})
