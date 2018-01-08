b3aca2638475f15ff2225e203d174a8cd803cb43
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-08 19:33:19 +0100

    add warning (c/p from styler).

--- R/styler.R
+++ R/styler.R
@@ -4,6 +4,10 @@
 #' [tidyverse style guide](http://style.tidyverse.org).
 #' @param strict Boolean indicating whether or not a strict version of styling
 #'   should be applied. See [styler::tidyverse_style()] for details.
+#' @section Warning:
+#' This function overwrites files (if styling results in a change of the
+#' code to be formatted). It is strongly suggested to only style files
+#' that are under version control or to create a backup copy.
 #' @return
 #' Invisibly returns a data frame that indicates for each file considered for
 #' styling whether or not it was actually changed.
