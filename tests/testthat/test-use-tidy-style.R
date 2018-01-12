f4b7f35cc76215bd58a731fcda1c56dbc321abe6
Lorenz Walthert <lorenz.walthert@icloud.com>
2018-01-12 23:53:59 +0100

    fiddle with tests...
    remove capture_output for now as suggested by reviewer to see what happens.

--- tests/testthat/test-use-tidy-style.R
+++ tests/testthat/test-use-tidy-style.R
@@ -2,10 +2,10 @@ context("use_tidy_style")
 
 test_that("styling the package works", {
   pkg <- scoped_temporary_package()
-  capture_output(use_r("bad_style"))
+  use_r("bad_style")
   path_to_bad_style <- proj_path("R/bad_style.R")
   write_utf8(path_to_bad_style, "a=2\n")
-  capture_output(use_tidy_style())
+  use_tidy_style()
   expect_identical(readLines(path_to_bad_style), "a <- 2")
   unlink(path_to_bad_style)
 })
