2d5000bc5256ccc4159afed2ca8d7b4679d9bcee
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-16 14:37:59 -0800

    Remove xref to devtools, which is not in DESCRIPTION

--- R/documentation.R
+++ R/documentation.R
@@ -3,7 +3,7 @@
 #' Adds a dummy `.R` file that will prompt roxygen to generate basic
 #' package-level documentation. If your package is named "foo", this will make
 #' help available to the user via `?foo` or `package?foo`. Once you call
-#' [devtools::document()], roxygen will flesh out the `.Rd` file using data from
+#' `devtools::document()`, roxygen will flesh out the `.Rd` file using data from
 #' the `DESCRIPTION`. That ensures you don't need to repeat the same information
 #' in multiple places. This `.R` file is also a good place for roxygen
 #' directives that apply to the whole package (vs. a specific function), such as
