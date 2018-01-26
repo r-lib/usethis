bf8e0746da3bdd58bfb664f9e2af029ed45266db
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-26 09:48:21 -0800

    Add failing test

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -22,3 +22,18 @@ test_that("nested project is disallowed, by default", {
   subdir <- create_directory(dir, "subdir")
   expect_error(scoped_temporary_project(subdir), "nested")
 })
+
+## https://github.com/r-lib/usethis/issues/227
+test_that("proj is normalized when path does not pre-exist", {
+  ## take care to provide a **non-absolute** path
+  path <- basename(tempfile())
+  withr::with_dir(
+    tempdir(), {
+      old_proj <- proj_get()
+      capture_output(create_package(path, rstudio = FALSE, open = FALSE))
+      new_proj <- proj_get()
+      proj_set(old_proj)
+    }
+  )
+  expect_true(dir.exists(new_proj))
+})
