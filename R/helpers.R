2c2a21d740c249f2e8201ecd6c8a828ab93b849f
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-02-15 12:54:55 -0800

    Consider existing dependencies in use_dependency()
    Fixes #99

--- R/helpers.R
+++ R/helpers.R
@@ -125,28 +125,67 @@ use_dependency <- function(package, type, version = "*") {
 
   if (package != "R" && !requireNamespace(package, quietly = TRUE)) {
     stop(
-      package, " must be installed before you can take a dependency on it",
+      value(package),
+      " must be installed before you can take a dependency on it",
       call. = FALSE
     )
   }
 
-  types <- c("Imports", "Depends", "Suggests", "Enhances", "LinkingTo")
+  types <- c("Depends", "Imports", "Suggests", "Enhances", "LinkingTo")
   names(types) <- tolower(types)
   type <- types[[match.arg(tolower(type), names(types))]]
 
   deps <- desc::desc_get_deps(proj_get())
 
-  matching_dep <- deps$package == package & deps$type == type
-  to_add <- !any(matching_dep)
-  to_set <- any(matching_dep & deps$version != version)
+  existing_dep <- deps$package == package
+  existing_type <- deps$type[existing_dep]
 
-  if (to_add) {
+  if (
+    !any(existing_dep) ||
+    (existing_type != "LinkingTo" && type == "LinkingTo")
+  ) {
     done("Adding ", value(package), " to ", field(type), " field in DESCRIPTION")
     desc::desc_set_dep(package, type, version = version, file = proj_get())
-  } else if (to_set) {
-    done("Setting ", value(package), " version to ", field(version), " field in DESCRIPTION")
-    desc::desc_set_dep(package, type, version = version, file = proj_get())
+    return(invisible())
   }
+
+  ## no downgrades
+  if (match(existing_type, types) < match(type, types)) {
+    warning(
+      "Package ", value(package), " is already listed in ",
+      value(existing_type), " in DESCRIPTION, no change made.",
+      call. = FALSE
+    )
+    return(invisible())
+  }
+
+  if (match(existing_type, types) > match(type, types)) {
+    if (existing_type != "LinkingTo") {
+      ## prepare for an upgrade
+      done(
+        "Removing ", value(package), " from ", field(existing_type),
+        " field in DESCRIPTION"
+      )
+      desc::desc_del_dep(package, existing_type, file = proj_get())
+    }
+  } else {
+    ## maybe change version?
+    to_version <- any(existing_dep & deps$version != version)
+    if (to_version) {
+      done(
+        "Setting ", value(package), " version to ",
+        field(version), " field in DESCRIPTION"
+      )
+      desc::desc_set_dep(package, type, version = version, file = proj_get())
+    }
+    return(invisible())
+  }
+
+  done(
+    "Adding ", value(package), " to ", field(type), " field in DESCRIPTION",
+    if (version != "*") ", with version ", field(version)
+  )
+  desc::desc_set_dep(package, type, version = version, file = proj_get())
 
   invisible()
 }
