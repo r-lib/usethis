e75aaaf73116efaaa059dee5485a7c3378bacb7c
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-02-17 12:00:37 -0800

    streamlining code

--- R/version.R
+++ R/version.R
@@ -3,14 +3,12 @@
 #' This increments the "Version" field in `DESCRIPTION`, adds a new heading
 #' to `NEWS.md` (if it exists), and then checks the result into git.
 #'
-#' @param level Which level to increment, must be one of "Mayor", "Minor",
-#' "Patch" or "Development". Defaults to NULL which activates interactive
+#' @param which Which level to increment, must be one of "dev", "patch",
+#' "minor" or "major". Defaults to NULL which activates interactive
 #' control.
-#' @param increment Numerical. How much to increment the level by.
-#' Defaults to 1.
 #'
 #' @export
-use_version <- function(level = NULL, increment = 1) {
+use_version <- function(which = NULL) {
   check_is_package("use_version()")
   if (uses_git() && git_uncommitted()) {
     stop(
@@ -19,42 +17,24 @@ use_version <- function(level = NULL, increment = 1) {
     )
   }
 
-  types <- c("Mayor", "Minor", "Patch", "Development")
+  types <- c("major", "minor", "patch", "dev")
 
-  ver <- desc::desc_get_version(proj_get())
+  ver_old <- desc::desc_get_version(proj_get())
 
-  if(is.null(level)) {
-    choice1 <- utils::menu(choices = types,
-                           title = paste0("Current version is ", paste0(ver),
-                                          " what will you increment?"))
-
-
-    increment_types <- c("1", "10", "custom")
-    choice2 <- utils::menu(choices = increment_types,
-                           title = paste0("How much will you increase by?"))
-
-    if(choice2 == 3) {
-      increment <- as.numeric(readline(prompt = "Enter custom increment: "))
-    } else if(choice2 == 2) {
-      increment <- 10
-    } else {
-      increment <- 1
-    }
-  } else {
-    choice1 <- pmatch(level, types)
+  if(is.null(which)) {
+    which <- utils::menu(choices = types,
+                         title = paste0("Current version is ", paste0(ver_old),
+                                        " what will you increment?"))
   }
 
-  ver[[1, choice1]] <- ver[[1, choice1]] + increment
+  suppressMessages(temp <- desc::desc_bump_version(which))
 
-  if(choice1 == 1) {
-    ver[[1, 2]] <- 0
-  }
-  if(choice1 < 3) {
-    ver[[1, 3]] <- 0
-  }
+  ver_new <- as.character(desc::desc_get_version(proj_get()))
 
-  use_description_field("Version", paste0(ver), overwrite = TRUE)
-  use_news_heading(paste0(ver))
+  suppressMessages(temp <- desc::desc_set_version(ver_old))
+
+  use_description_field("Version", paste0(ver_new), overwrite = TRUE)
+  use_news_heading(paste0(ver_new))
   git_check_in(
     base_path = proj_get(),
     paths = c("DESCRIPTION", "NEWS.md"),
--- tests/testthat/test-use-version.R
+++ tests/testthat/test-use-version.R
@@ -1,22 +1,17 @@
 context("use_version.R")
 
-test_that("use_version() requires a package", {
-  scoped_temporary_project()
-  expect_error(use_version(), "not an R package")
-})
-
 test_that("use_version() increments Mayor by 1 and resets other fields", {
   # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
   skip_if(getRversion() < 3.2)
 
   scoped_temporary_package()
   capture_output(
-    use_description_field(name = "Version", value = "0.1.1.9000", overwrite = TRUE)
+    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
   )
-  capture_output(use_version(level = "Mayor"))
+  capture_output(use_version("major"))
   expect_identical(
     as.character(desc::desc_get_version(proj_get())),
-    "1.0.0.9000"
+    "2.0.0"
   )
 })
 
@@ -28,7 +23,7 @@ test_that("use_version() increments Minor by 1 and only resets Patch", {
   capture_output(
     use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
   )
-  capture_output(use_version(level = "Minor"))
+  capture_output(use_version("minor"))
   expect_identical(
     as.character(desc::desc_get_version(proj_get())),
     "1.2.0.9000"
@@ -43,9 +38,15 @@ test_that("use_version() increments patch by 1 leaving other fields alone", {
   capture_output(
     use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
   )
-  capture_output(use_version(level = "Patch"))
+  capture_output(use_version("patch"))
   expect_identical(
     as.character(desc::desc_get_version(proj_get())),
     "1.1.2.9000"
   )
 })
+
+test_that("use_version() requires a package", {
+  scoped_temporary_project()
+  expect_error(use_version("major"), "not an R package")
+})
+
