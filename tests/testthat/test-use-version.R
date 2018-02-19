efa75001502b486093e062a889b8b931d1d83558
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2018-02-19 10:07:25 -0800

    testing bump_version case-by-case

--- tests/testthat/test-use-version.R
+++ tests/testthat/test-use-version.R
@@ -1,6 +1,38 @@
 context("use_version.R")
 
-test_that("use_version() increments Mayor by 1 and resets other fields", {
+test_that("bump_version() increments Major by 1 and resets other fields", {
+
+  scoped_temporary_package()
+  capture_output(
+    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
+  )
+  expect_identical(
+    as.character(bump_version(desc::desc_get_version(proj_get()), "major")),
+    "2.0.0"
+  )
+  expect_identical(
+    as.character(bump_version(desc::desc_get_version(proj_get()), "minor")),
+    "1.2.0"
+  )
+  expect_identical(
+    as.character(bump_version(desc::desc_get_version(proj_get()), "patch")),
+    "1.1.2"
+  )
+  expect_identical(
+    as.character(bump_version(desc::desc_get_version(proj_get()), "dev")),
+    "1.1.1.9001"
+  )
+
+  capture_output(
+    use_description_field(name = "Version", value = "1.1.1", overwrite = TRUE)
+  )
+  expect_identical(
+    as.character(bump_version(desc::desc_get_version(proj_get()), "dev")),
+    "1.1.1.9000"
+  )
+})
+
+test_that("use_version() increments major and resets other fields to 0", {
   # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
   skip_if(getRversion() < 3.2)
 
@@ -15,83 +47,7 @@ test_that("use_version() increments Mayor by 1 and resets other fields", {
   )
 })
 
-test_that("use_version() increments Minor by 1 and only resets Patch", {
-  # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
-  skip_if(getRversion() < 3.2)
-
-  scoped_temporary_package()
-  capture_output(
-    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
-  )
-  capture_output(use_version("minor"))
-  expect_identical(
-    as.character(desc::desc_get_version(proj_get())),
-    "1.2.0"
-  )
-})
-
-test_that("use_version() increments patch by 1 leaving other fields alone", {
-  # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
-  skip_if(getRversion() < 3.2)
-
-  scoped_temporary_package()
-  capture_output(
-    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
-  )
-  capture_output(use_version("patch"))
-  expect_identical(
-    as.character(desc::desc_get_version(proj_get())),
-    "1.1.2"
-  )
-})
-
-test_that("use_version() increments patch by 1 leaving other fields alone", {
-  # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
-  skip_if(getRversion() < 3.2)
-
-  scoped_temporary_package()
-  capture_output(
-    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
-  )
-  capture_output(use_version("patch"))
-  expect_identical(
-    as.character(desc::desc_get_version(proj_get())),
-    "1.1.2"
-  )
-})
-
-test_that("use_version() increments dev to .9000", {
-  # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
-  skip_if(getRversion() < 3.2)
-
-  scoped_temporary_package()
-  capture_output(
-    use_description_field(name = "Version", value = "1.1.1", overwrite = TRUE)
-  )
-  capture_output(use_version("dev"))
-  expect_identical(
-    as.character(desc::desc_get_version(proj_get())),
-    "1.1.1.9000"
-  )
-})
-
-test_that("use_version() increments dev to .9000", {
-  # git2r::git2r::discover_repository() not working on R 3.1 (Travis)
-  skip_if(getRversion() < 3.2)
-
-  scoped_temporary_package()
-  capture_output(
-    use_description_field(name = "Version", value = "1.1.1.9000", overwrite = TRUE)
-  )
-  capture_output(use_version("dev"))
-  expect_identical(
-    as.character(desc::desc_get_version(proj_get())),
-    "1.1.1.9001"
-  )
-})
-
 test_that("use_version() requires a package", {
   scoped_temporary_project()
   expect_error(use_version("major"), "not an R package")
 })
-
