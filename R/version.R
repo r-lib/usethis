d194d414d40d33c8b0834fab66885388bee382de
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-02-19 23:38:49 -0800

    Move use_dev_version()

--- R/version.R
+++ R/version.R
@@ -46,6 +46,35 @@ use_version <- function(which = NULL) {
   invisible(TRUE)
 }
 
+#' Increment to a development version
+#'
+#' This adds ".9000" to the "Version" field in `DESCRIPTION`, adds a new heading
+#' to `NEWS.md` (if it exists), and then checks the result into git.
+#'
+#' @export
+use_dev_version <- function() {
+  check_is_package("use_dev_version()")
+  check_uncommitted_changes()
+
+  ver <- desc::desc_get_version(proj_get())
+  if (length(unlist(ver)) > 3) {
+    return(invisible())
+  }
+
+  dev_ver <- paste0(ver, ".9000")
+
+  use_description_field("Version", dev_ver, overwrite = TRUE)
+  use_news_heading(dev_ver)
+  git_check_in(
+    base_path = proj_get(),
+    paths = c("DESCRIPTION", "NEWS.md"),
+    message = "Use development version"
+  )
+
+  invisible(TRUE)
+}
+
+
 bump_version <- function(ver) {
   bumps <- c("major", "minor", "patch", "dev")
   vapply(bumps, bump_, character(1), ver = ver)
