93a7720e635b22fd85ab9097479da6db1180c018
martinjhnhadley <martinjohnhadley@gmail.com>
2018-04-04 16:15:44 +0100

    template for use_tibble()

---
+++ inst/templates/tibble.R
@@ -0,0 +1,9 @@
+#' Enable tibble printing for tibbles attached by a package
+#'
+#' See \code{tibble::\link[tibble]{tibble}} for details.
+#'
+#' @name tibble
+#' @keywords internal
+#' @export
+#' @importFrom tibble tibble
+NULL
