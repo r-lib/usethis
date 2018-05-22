5ca12eb5017189f1cfaceadecd69091220d8e537
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-05-22 15:32:43 -0700

    Target current project in spell_check_setup()

--- R/spelling.R
+++ R/spelling.R
@@ -14,6 +14,8 @@ use_spell_check <- function(vignettes = TRUE, lang = "en-US", error = FALSE) {
   check_installed("spelling")
   use_dependency("spelling", "Suggests")
   use_description_field("Language", lang)
-  spelling::spell_check_setup(vignettes = vignettes, lang = lang, error = error)
+  spelling::spell_check_setup(
+    pkg = proj_get(), vignettes = vignettes, lang = lang, error = error
+  )
   todo("Run devtools::check() to test")
 }
