0269a9d6921ff3a9c7dae4b350b21bbe5839cb95
Jim Hester <james.f.hester@gmail.com>
2018-05-24 15:57:49 -0400

    Add use_logo function to add a logo to a package
    Fixes #358

---
+++ R/logo.R
@@ -0,0 +1,49 @@
+#' README logo_
+#'
+#' These helpers produce the markdown text you need in your README to include
+#' package logos.
+#'
+#' @param img The path to an existing image file
+#' @inheritParams magick::image_resize
+#'
+#' @examples
+#' \dontrun{
+#' use_logo("usethis.png")
+#' }
+#' @export
+use_logo <- function(img, geometry = "120x140") {
+  if (has_logo()) {
+    return(invisible(FALSE))
+  }
+
+  check_installed("magick")
+  check_is_package("use_logo()")
+
+  img_data <- magick::image_read(img)
+  img_data <- magick::image_resize(img_data, geometry)
+
+  fs::dir_create(proj_path("man", "figures"))
+  magick::image_write(img_data, proj_path("man", "figures", "logo.png"))
+
+  done(
+    "Resized ", basename(img), " to ", geometry
+  )
+
+  todo(
+    "Add a logo by adding the following line to your README:"
+  )
+  pkg <- project_name()
+
+  img <- paste0("# ", pkg, " <img src=\"man/figures/logo.png\" align=\"right\" />")
+  code_block(img)
+}
+
+has_logo <- function() {
+  readme_path <- proj_path("README.md")
+  if (!fs::file_exists(readme_path)) {
+    return(FALSE)
+  }
+
+  readme <- readLines(readme_path)
+  any(grepl("<img src=\"man/figures/logo.png\" align=\"right\" />", readme, fixed = TRUE))
+}
