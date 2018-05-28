244e43af493143d792fe52d908fffdedd4966511
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2018-05-28 09:50:07 -0700

    Don't help people depend on tidyverse or devtools (#365)
    * Don't help people depend on tidyverse or devtools
    
    Fixes #172
    
    * Don't help people depend on the dev version either!
    
    * Too early to do this for devtools
    
    Revisit when more of the underlying packages have made it to CRAN

---
+++ tests/testthat/test-use-package.R
@@ -0,0 +1,6 @@
+context("use_package")
+
+test_that("use_package() won't facilitate dependency on tidyverse", {
+  scoped_temporary_package()
+  expect_error(use_package("tidyverse"), "rarely a good idea")
+})
