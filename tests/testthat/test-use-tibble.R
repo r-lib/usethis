4e657e61c8024427f1a7e8a5c97ea33b46a31b18
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2018-06-05 19:11:59 -0700

    Refashion use_tibble() (#383)
    * Refashion use_tibble()
    
    Fixes #382
    
    * Punctuation

--- tests/testthat/test-use-tibble.R
+++ tests/testthat/test-use-tibble.R
@@ -5,13 +5,12 @@ test_that("use_tibble() requires a package", {
   expect_error(use_tibble(), "not an R package")
 })
 
-test_that("use_tibble() adds promised file, Imports tibble", {
+test_that("use_tibble() Imports tibble", {
   with_mock(
     `usethis:::uses_roxygen` = function(base_path) TRUE, {
       scoped_temporary_package()
-      capture_output(use_tibble())
+      expect_output(use_tibble(), "#' @importFrom tibble tibble")
       expect_match(desc::desc_get("Imports", proj_get()), "tibble")
-      expect_true(file.exists(proj_path("R", "utils-tibble.R")))
     }
   )
 })
