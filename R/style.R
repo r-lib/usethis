453a57bef470a4effd4948949500eaed0856892e
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-06-21 08:24:17 -0700

    Make style.R more self-explanatory

--- R/style.R
+++ R/style.R
@@ -1,17 +1,18 @@
-todo_bullet <- function() crayon::red(clisymbols::symbol$bullet)
-done_bullet <- function() crayon::green(clisymbols::symbol$tick)
+# Helpers --------------------------------------------------------------------
 
-## generates one line
+## anticipates usage where the `...` bits make up one line
 cat_line <- function(...) {
   cat(..., "\n", sep = "")
 }
 
-## generates one bulletized line
+todo_bullet <- function() crayon::red(clisymbols::symbol$bullet)
+done_bullet <- function() crayon::green(clisymbols::symbol$tick)
+
+## adds a leading bullet
 bulletize <- function(line, bullet = "*") {
   line <- paste0(bullet, " ", line)
 }
 
-## in the name of standardizing on glue
 collapse <- function(x, sep = ", ", width = Inf, last = "") {
   glue::glue_collapse(x, sep = sep, width = Inf, last = last)
 }
@@ -21,6 +22,8 @@ glue_lines <- function(lines, .envir = parent.frame()) {
   unlist(lapply(lines, glue, .envir = .envir))
 }
 
+
+# Functions designed for a single line ----------------------------------------
 todo <- function(..., .envir = parent.frame()) {
   out <- glue(collapse(c(...), sep = ""), .envir = .envir)
   cat_line(bulletize(out, bullet = todo_bullet()))
@@ -31,7 +34,10 @@ done <- function(..., .envir = parent.frame()) {
   cat_line(bulletize(out, bullet = done_bullet()))
 }
 
-## each individual bit of `...` is destined to be a line
+
+# Function designed for several lines -------------------------------------
+
+## ALERT: each individual bit of `...` is destined to be a line
 code_block <- function(..., copy = interactive(), .envir = parent.frame()) {
   lines <- glue_lines(c(...), .envir = .envir)
   block <- paste0("  ", lines, collapse = "\n")
@@ -43,6 +49,10 @@ code_block <- function(..., copy = interactive(), .envir = parent.frame()) {
 }
 
 
+# Inline styling functions ------------------------------------------------
+
+## use these inside todo(), done(), and code_block()
+## ^^ and let these functions handle any glue()ing ^^
 field <- function(...) {
   x <- paste0(...)
   crayon::green(x)
