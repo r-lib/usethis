07fe5ee0b586eb05a45d71253f8b799ce6b54de6
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2018-08-08 13:57:26 -0700

    Update use_tidy_eval() (#377)
    * Link to main rlang pkgdown site
    
    Fixes #343
    
    * Remove UQ/UQS from the imports
    
    Fixes #419
    
    * Update the tidy eval template
    
    Adapting from https://github.com/tidyverse/dplyr/blob/master/R/utils-tidy-eval.R
    
    * Check project is a package
    
    * Import individual functions from rlang, not the entire namespace

--- R/tidyverse.R
+++ R/tidyverse.R
@@ -137,10 +137,9 @@ dep_version <- function(x) {
 #' @export
 #' @rdname tidyverse
 use_tidy_eval <- function() {
-  if (!uses_roxygen()) {
-    stop_glue("{code('use_tidy_eval()')} requires that you use roxygen.")
-  }
+  check_is_package("use_tidy_eval()")
 
+  use_dependency("roxygen2", "Suggests")
   use_dependency("rlang", "Imports", ">= 0.1.2")
   use_template("tidy-eval.R", "R/utils-tidy-eval.R")
 
