3b7dfdc074f79ab7d9b92dd40f037472ddd8246d
hadley <h.wickham@gmail.com>
2018-11-26 17:09:30 -0600

    Initial push attempt

--- R/pr.R
+++ R/pr.R
@@ -1,5 +1,5 @@
 pr_use <- function(branch) {
-  check_uses_git()
+  check_uses_github()
 
   cur_branch <- proj_git_branch_get()
   if (cur_branch$name == branch) {
@@ -25,16 +25,38 @@ pr_use <- function(branch) {
   invisible()
 }
 
+
+pr_push <- function() {
+  check_on_branch()
+  check_uses_github()
+  check_uncommitted_changes()
+
+  done("Pushing to GitHub")
+  git2r::push(proj_git_repo(), "origin", paste0("refs/heads/", cur_branch$name))
+
+  # TODO: figure out how to determine if PR already exists
+  url <- glue("https://github.com/{github_ower()}/{github_repo()}/compare/pkgman?expand=1")
+  view_url(url)
+}
+
 # Helpers -----------------------------------------------------------------
 
+check_on_branch <- function() {
+  cur_branch <- proj_git_branch_get()
+  if (cur_branch$name == "master") {
+    stop("Currently on master branch. Do you need to call `pr_use()` first?", call. = FALSE)
+  }
+}
+
 proj_git_repo <- function() {
+  check_uses_git()
   git2r::repository(proj_path())
 }
 
 proj_git_branch_get <- function() {
   repo <- proj_git_repo()
   branch <- git2r::repository_head(repo)
-  if (!git2r::is_branch(cur_branch)) {
+  if (!git2r::is_branch(branch)) {
     stop("Detached head; can't continue", call. = FALSE)
   }
 
