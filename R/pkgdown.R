98a55a8614e2c73940d6bf3d9eb9f71ca32e60c6
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-11-26 11:06:58 -0700

    Updates in response to review

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -2,7 +2,7 @@
 #'
 #' [pkgdown](https://github.com/r-lib/pkgdown) makes it easy to turn your
 #' package into a beautiful website. This helper creates a pkgdown config file
-#' and adds the file and destination diretory to `.Rbuildignore`
+#' and adds the file and destination directory to `.Rbuildignore`
 #'
 #' @seealso http://pkgdown.r-lib.org/articles/pkgdown.html#configuration
 #'
@@ -10,9 +10,13 @@
 #' @param dst_path target directory for pkgdown docs
 #'
 #' @export
-use_pkgdown <- function(config_file = "_pkgdown.yml", dst_path = "docs") {
+use_pkgdown <- function(config_file = "_pkgdown.yml", destdir = "docs") {
   check_is_package("use_pkgdown()")
-  use_build_ignore(c(config_file, dst_path))
+  use_build_ignore(c(config_file, destdir))
 
-  edit_file(proj_path(config_file))
+  config <- proj_path(config_file)
+
+  write_over(config, paste("destination:", destdir))
+
+  edit_file(config)
 }
