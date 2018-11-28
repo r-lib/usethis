8d609fe9c77cdb6d10fa7815463a659543e3f335
hadley <h.wickham@gmail.com>
2018-11-28 13:40:08 -0600

    Implement use_conflicted
    Fix #362

--- R/rprofile.R
+++ R/rprofile.R
@@ -1,10 +1,12 @@
 #' Helpers to make useful changes to `.Rprofile`
 #'
+#' @description
 #' All functions open your `.Rprofile` and gives you the code you need to
 #' paste in.
-#' * `use_usethis()`: makes usethis available in interactive sessions.
-#' * `use_partial_warning()`: warn on partial matches.
 #'
+#' * `use_conflicted()`:  makes conflicted available in interactive sessions.
+#' * `use_partial_warning()`: warn on partial matches.
+#' * `use_usethis()`: makes usethis available in interactive sessions.
 #' @name rprofile-helper
 NULL
 
@@ -40,5 +42,24 @@ use_partial_warnings <- function() {
     "  warnPartialMatchDollar = TRUE,",
     "  warnPartialMatchAttr = TRUE",
     ")"
+  )
+}
+
+
+
+#' @rdname rprofile-helper
+#' @export
+use_conflicted <- function() {
+  edit_r_profile("user")
+
+  todo(
+    "Include this code in {value('.Rprofile')} to use {field('conflicted')} ",
+    "available in all interactive sessions"
+  )
+  ## in glue, you get a literal brace by using doubled braces
+  code_block(
+    "if (interactive()) {{",
+    "  suppressMessages(require(conflicted))",
+    "}}"
   )
 }
