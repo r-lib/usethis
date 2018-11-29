0060709cd13721f8a9a70bdef72a27e9b53505bf
Hadley Wickham <h.wickham@gmail.com>
2018-11-29 17:34:32 -0600

    Refactoring use_rcpp (#506)

--- R/rcpp.R
+++ R/rcpp.R
@@ -5,26 +5,25 @@
 #' @export
 use_rcpp <- function() {
   check_is_package("use_rcpp()")
+  check_uses_roxygen("use_rcpp()")
+
+  use_src()
+
   use_dependency("Rcpp", "LinkingTo")
   use_dependency("Rcpp", "Imports")
+  roxygen_ns_append("@importFrom Rcpp sourceCpp") && roxygen_update()
+
+  invisible()
+}
+
+use_src <- function() {
+  check_is_package("use_src()")
+  check_uses_roxygen("use_rcpp()")
 
   use_directory("src")
   use_git_ignore(c("*.o", "*.so", "*.dll"), "src")
+  roxygen_ns_append(glue("@useDynLib {project_name()}, .registration = TRUE")) &&
+    roxygen_update()
 
-  if (uses_roxygen()) {
-    todo("Include the following roxygen tags somewhere in your package")
-    code_block(
-      "#' @useDynLib {project_name()}, .registration = TRUE",
-      "#' @importFrom Rcpp sourceCpp",
-      "NULL"
-    )
-  } else {
-    todo("Include the following directives in your NAMESPACE")
-    code_block(
-      "useDynLib('{project_name()}', .registration = TRUE)",
-      "importFrom('Rcpp', 'sourceCpp')"
-    )
-    edit_file(proj_path("NAMESPACE"))
-  }
-  todo("Run {code('devtools::document()')}")
+  invisible()
 }
