d48f8e29d67ad264e522b04ef4a4bf9bfebf203b
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-11-29 09:07:08 -0700

    Make use_pkgdown configurable (#467)
    Fixes #392

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -1,14 +1,22 @@
 #' Use pkgdown
 #'
-#' [pkgdown](https://github.com/hadley/pkgdown) makes it easy to turn your
-#' package into a beautiful website. This helper creates `_pkgdown.yml`
-#' and `docs/` for you, and adds them to `.Rbuildignore`
+#' [pkgdown](https://github.com/r-lib/pkgdown) makes it easy to turn your
+#' package into a beautiful website. This helper creates a pkgdown config file
+#' and adds the file and destination directory to `.Rbuildignore`
+#'
+#' @seealso http://pkgdown.r-lib.org/articles/pkgdown.html#configuration
+#'
+#' @param config_file pkgdown yaml config file
+#' @param dst_path target directory for pkgdown docs
 #'
 #' @export
-use_pkgdown <- function() {
+use_pkgdown <- function(config_file = "_pkgdown.yml", destdir = "docs") {
   check_is_package("use_pkgdown()")
-  edit_file(proj_path("_pkgdown.yml"))
-  use_build_ignore("_pkgdown.yml")
+  use_build_ignore(c(config_file, destdir))
 
-  use_directory("docs", ignore = TRUE)
+  config <- proj_path(config_file)
+
+  write_over(config, paste("destination:", destdir))
+
+  edit_file(config)
 }
