0cb681b44be06f0b1b68f592c0631ac4db661416
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-12-19 19:09:37 -0800

    Correct function name

--- R/release.R
+++ R/release.R
@@ -64,7 +64,7 @@ release_checklist <- function(version) {
     "",
     "Wait for CRAN...",
     "",
-    todo("`usethis::use_release_tag()`"),
+    todo("`usethis::use_github_release()`"),
     todo("`usethis::use_dev_version()`"),
     todo("`usethis::use_news()`", !on_cran),
     todo("Update install instructions in README", !on_cran),
