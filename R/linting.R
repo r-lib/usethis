99648964eab2932215b019b501b0719aad86532a
leonawicz <matt_leonawicz@esource.com>
2019-02-15 09:03:39 -0700

    Update function to use usethis-consistent helpers. Add initial unit tests.

--- R/linting.R
+++ R/linting.R
@@ -9,27 +9,25 @@ use_lintr <- function(type = c("local", "travis", "test")){
   type <- match.arg(type)
 
   if(type == "test"){
-    dir.create("inst", showWarnings = FALSE)
+    create_directory("inst")
     .lintr <- "inst/.lintr"
   } else {
     .lintr <- ".lintr"
   }
-
-  sink(.lintr)
-  cat(paste0("linters: with_defaults(\n  ", "line_length_linter(80))\n"))
-  sink()
+  write_over(.lintr, c("linters: with_defaults(", "  line_length_linter(80))"))
 
   if(type == "test" && !file.exists(".lintr")) file.symlink("inst/.lintr", ".lintr")
   use_build_ignore(".lintr")
   if(type == "test"){
     use_build_ignore("inst/.lintr")
-    sink("tests/testthat/test-lintr.R")
-    cat(paste0("if (requireNamespace(\"lintr\", quietly = TRUE)) {\n  ",
-               "context(\"lints\")\n  ",
-               "test_that(\"Package Style\", {\n    ",
-               "lintr::expect_lint_free()\n  ",
-               "})\n", "}\n"))
-    sink()
+    write_over(
+      "tests/testthat/test-lintr.R",
+      c("if (requireNamespace(\"lintr\", quietly = TRUE)) {",
+        "  context(\"lints\")",
+        "  test_that(\"Package Style\", {",
+        "    lintr::expect_lint_free()",
+        "  })", "}")
+    )
     ui_todo("Code linting set up as a unit test. Edit {ui_path('inst/.lintr')} as needed.")
   } else if(type == "travis"){
     ui_todo("Insert the following code in {ui_path('.travis.yml')}")
