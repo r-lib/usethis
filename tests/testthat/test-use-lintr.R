4801ba7f5e4b64e01aaa4d83913dd24f330cc823
leonawicz <matt_leonawicz@esource.com>
2019-02-15 09:27:23 -0700

    Switch scoped temp project to package.

--- tests/testthat/test-use-lintr.R
+++ tests/testthat/test-use-lintr.R
@@ -14,11 +14,11 @@ test_that("use_lintr('travis') creates build-ignored, promised file", {
   expect_proj_file(".lintr")
 })
 
-# test_that("use_lintr('test') creates build-ignored, promised files", {
-#   scoped_temporary_project()
-#   use_lintr("test")
-#   expect_true(is_build_ignored("^\\.lintr$"))
-#   expect_true(is_build_ignored("^inst/\\.lintr$"))
-#   expect_proj_file("inst/.lintr")
-#   expect_proj_file(".lintr")
-# })
+test_that("use_lintr('test') creates build-ignored, promised files", {
+  scoped_temporary_package()
+  use_lintr("test")
+  expect_true(is_build_ignored("^\\.lintr$"))
+  expect_true(is_build_ignored("^inst/\\.lintr$"))
+  expect_proj_file("inst/.lintr")
+  expect_proj_file(".lintr")
+})
