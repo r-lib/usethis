099ec7f21a329dd528bf394534b6827d472a10c8
James Balamuta <balamut2@illinois.edu>
2019-02-16 21:11:14 -0600

    Make the use_rcpp_armadillo unit test more robust by not testing compilation flags but the existence of both Makevar files.

--- tests/testthat/test-use-rcpp.R
+++ tests/testthat/test-use-rcpp.R
@@ -1,4 +1,4 @@
-context("use-rcpp.R")
+context("use_rcpp")
 
 test_that("use_rcpp() requires a package", {
   scoped_temporary_project()
@@ -24,18 +24,9 @@ test_that("use_rcpp_armadillo() creates Makevars files and edits DESCRIPTION", {
 
   use_rcpp_armadillo()
   expect_match(desc::desc_get("LinkingTo", pkg), "RcppArmadillo")
+  expect_proj_file("src", "Makevars")
+  expect_proj_file("src", "Makevars.win")
 
-  makevars_settings <- c(
-    "CXX_STD = CXX11",
-    "PKG_CXXFLAGS = $(SHLIB_OPENMP_CXXFLAGS)",
-    "PKG_LIBS = $(SHLIB_OPENMP_CXXFLAGS) $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)"
-  )
-
-  makevars <- readLines(proj_path("src", "Makevars"))
-  expect_true(all(makevars_settings %in% makevars))
-
-  makevars_win <- readLines(proj_path("src", "Makevars.win"))
-  expect_true(all(makevars_settings %in% makevars_win))
 })
 
 test_that("use_rcpp_eigen() edits DESCRIPTION", {
@@ -44,7 +35,6 @@ test_that("use_rcpp_eigen() edits DESCRIPTION", {
 
   use_rcpp_eigen()
   expect_match(desc::desc_get("LinkingTo", pkg), "RcppEigen")
-  expect_match(desc::desc_get("Imports", pkg), "RcppEigen")
 })
 
 test_that("use_src() doesn't message if not needed", {
