844c735895d4622f5d962a6f3147b9c5b6a39143
Hadley Wickham <h.wickham@gmail.com>
2019-02-18 17:23:39 -0600

    Revert "Add use_rcpp_armadillo() and use_rcpp_eigen() (#601)"
    This reverts commit d6f6ead19b84e43f6593da09f8bbda65629e6dc1.

--- tests/testthat/test-use-rcpp.R
+++ tests/testthat/test-use-rcpp.R
@@ -1,4 +1,4 @@
-context("use_rcpp")
+context("use-rcpp.R")
 
 test_that("use_rcpp() requires a package", {
   scoped_temporary_project()
@@ -18,25 +18,6 @@ test_that("use_rcpp() creates files/dirs, edits DESCRIPTION and .gitignore", {
   expect_true(all(c("*.o", "*.so", "*.dll") %in% ignores))
 })
 
-test_that("use_rcpp_armadillo() creates Makevars files and edits DESCRIPTION", {
-  pkg <- scoped_temporary_package()
-  use_roxygen_md()
-
-  use_rcpp_armadillo()
-  expect_match(desc::desc_get("LinkingTo", pkg), "RcppArmadillo")
-  expect_proj_file("src", "Makevars")
-  expect_proj_file("src", "Makevars.win")
-
-})
-
-test_that("use_rcpp_eigen() edits DESCRIPTION", {
-  pkg <- scoped_temporary_package()
-  use_roxygen_md()
-
-  use_rcpp_eigen()
-  expect_match(desc::desc_get("LinkingTo", pkg), "RcppEigen")
-})
-
 test_that("use_src() doesn't message if not needed", {
   scoped_temporary_package()
   use_roxygen_md()
@@ -47,41 +28,3 @@ test_that("use_src() doesn't message if not needed", {
 
   expect_silent(use_src())
 })
-
-test_that("use_makevars() respects pre-existing Makevars", {
-  pkg <- scoped_temporary_package()
-
-  dir_create(proj_path("src"))
-  makevars_file <- proj_path("src", "Makevars")
-  makevars_win_file <- proj_path("src", "Makevars.win")
-
-  writeLines("USE_CXX = CXX11", makevars_file)
-  file_copy(makevars_file, makevars_win_file)
-
-  before_makevars_file <- readLines(makevars_file)
-  before_makevars_win_file <- readLines(makevars_win_file)
-
-  makevars_settings <- list(
-    "PKG_CXXFLAGS" = "-Wno-reorder"
-  )
-  use_makevars(makevars_settings)
-
-  expect_identical(before_makevars_file, readLines(makevars_file))
-  expect_identical(before_makevars_win_file, readLines(makevars_win_file))
-})
-
-test_that("use_makevars() creates Makevars files with appropriate configuration", {
-  pkg <- scoped_temporary_package()
-
-  makevars_settings <- list(
-    "CXX_STD"="CXX11"
-  )
-  use_makevars(makevars_settings)
-
-  makevars_content <- paste0(names(makevars_settings)," = ", makevars_settings)
-
-  expect_identical(makevars_content, readLines(proj_path("src", "Makevars")))
-  expect_identical(makevars_content, readLines(proj_path("src", "Makevars.win")))
-})
-
-
