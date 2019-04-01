a330ed9c39fd4d76b0d2f1bbf32d32cdd8eb8c18
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-04-01 09:46:04 -0700

    Transition towards path_package() instead of system.file()
    Fixes #572
    Relates to #347
    Closes #684
    
    See https://github.com/r-lib/fs/commit/de56a640e874b642d002108d4bb3f67f89bf7843

--- R/package.R
+++ R/package.R
@@ -89,7 +89,7 @@ how_to_use <- function(package, type) {
   switch(type,
     imports = ui_todo("Refer to functions with {ui_code(paste0(package, '::fun()'))}"),
     depends = ui_todo(
-      "Are you sure you want {ui_field('Depends')}?\\
+      "Are you sure you want {ui_field('Depends')}? \\
       {ui_field('Imports')} is almost always the better choice."
     ),
     suggests = {
@@ -104,7 +104,7 @@ how_to_use <- function(package, type) {
 }
 
 show_includes <- function(package) {
-  incl <- system.file("include", package = package)
+  incl <- path_package("include", package = package)
   h <- dir_ls(incl, regexp = "[.](h|hpp)$")
   if (length(h) == 0) return()
 
