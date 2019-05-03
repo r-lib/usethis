e1775b8ef3d1672f5f70a47ae9f7d6e05b2c59e9
Ryan Price <ryapric@gmail.com>
2019-05-03 13:36:27 -0500

    Fix typos in seealso.

--- R/jenkins.R
+++ R/jenkins.R
@@ -5,8 +5,8 @@
 #' calling this function will also run `use_make()` if a Makefile does not
 #' already exist at the project root.
 #'
-#' @seealso The [documentation on Jekins
-#'   Pipelines](https://jenkins.io/doc/book/pipeline/jenkinsfile/).
+#' @seealso The [documentation on Jenkins
+#'   Pipelines](https://jenkins.io/doc/book/pipeline/jenkinsfile).
 #' @seealso [use_make()]
 #' @export
 use_jenkins <- function() {
