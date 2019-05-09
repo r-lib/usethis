f62fce9937fc7d4f4bc1604d060f196a2123b9b9
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-10 00:44:14 +0200

    Add use_tidy_versions() to release issue

--- R/release.R
+++ R/release.R
@@ -64,6 +64,7 @@ release_checklist <- function(version) {
     "Submit to CRAN:",
     "",
     todo("`usethis::use_version('{type}')`"),
+    todo('`usethis_use_tidy_versions(TRUE, "CRAN")`'),
     todo("Update `cran-comments.md`"),
     todo("`devtools::submit_cran()`"),
     todo("Approve email"),
