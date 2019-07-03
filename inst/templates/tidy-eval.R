e974abb6824ed2f901a22dbab389f5d05852d15b
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-07-03 13:00:52 -0700

    Manually restore fixes from this PR

--- inst/templates/tidy-eval.R
+++ inst/templates/tidy-eval.R
@@ -6,13 +6,13 @@
 #'   \code{\link[rlang:quotation]{syms}()} creates a list of symbols from a
 #'   character vector.
 #'
-#' * \code{\link[rlang]{enquo}()} and \code{\link[rlang]{enquos}()}
-#'   delay the execution of one or several function arguments.
-#'   \code{enquo()} returns a single quoted expression, which is like
-#'   a blueprint for the delayed computation. \code{enquos()} returns
-#'   a list of such quoted expressions.
+#' * \code{\link[rlang:quotation]{enquo}()} and
+#'   \code{\link[rlang:quotation]{enquos}()} delay the execution of one or
+#'   several function arguments. \code{enquo()} returns a single quoted
+#'   expression, which is like a blueprint for the delayed computation.
+#'   \code{enquos()} returns a list of such quoted expressions.
 #'
-#' * \code{\link[rlang]{expr}()} quotes a new expression _locally_. It
+#' * \code{\link[rlang:quotation]{expr}()} quotes a new expression _locally_. It
 #'   is mostly useful to build new expressions around arguments
 #'   captured with [enquo()] or [enquos()]:
 #'   \code{expr(mean(!!enquo(arg), na.rm = TRUE))}.
