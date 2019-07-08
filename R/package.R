95a28c0f4221e65d4237a1b7ae1b0fa02e5c5165
vincent guyader <vincent@guyader.pro>
2019-07-08 11:33:26 +0200

    allow 'tidyverse' in use_package if type='depends' (For data analysis… (#809)
    * allow 'tidyverse' in use_package if type='depends' (For data analysis projects). fix #760
    
    * Tweak message

--- R/package.R
+++ R/package.R
@@ -22,7 +22,10 @@
 #' use_dev_package("glue")
 #' }
 use_package <- function(package, type = "Imports", min_version = NULL) {
-  refuse_package(package, verboten = "tidyverse")
+
+  if (type == "Imports") {
+    refuse_package(package, verboten = "tidyverse")
+  }
 
   use_dependency(package, type, min_version = min_version)
   how_to_use(package, type)
@@ -73,10 +76,15 @@ package_remote <- function(package) {
 
 refuse_package <- function(package, verboten) {
   if (identical(package, verboten)) {
+    code <- glue("use_package(\"{package}\", type = \"depends\")")
     ui_stop(
       "{ui_value(package)} is a meta-package and it is rarely a good idea to \\
       depend on it. Please determine the specific underlying package(s) that \\
-      offer the function(s) you need and depend on that instead."
+      offer the function(s) you need and depend on that instead. \\
+      For data analysis projects that use a package structure but do not implement \\
+      a formal R package, adding {ui_value(package)} to Depends is a \\
+      reasonable compromise. Call {ui_code(code)} to achieve this.
+      "
     )
   }
   invisible(package)
