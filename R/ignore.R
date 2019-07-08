2f0416eb605a0b1a75a9ce089d087cad897b33fe
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-07-08 12:25:41 +0200

    Add example to build-ignore all files with a given extension

--- R/ignore.R
+++ R/ignore.R
@@ -6,10 +6,18 @@
 #' expression that will only match that path. Repeated entries will be
 #' silently removed.
 #'
+#' `use_build_ignore` is designed to ignore *individual* files. If you
+#' want to ignore *all* files with a given extension, consider providing
+#' an "as-is" regular expression, using `escape = FALSE`; see examples.
+#'
 #' @param files Character vector of path names.
 #' @param escape If `TRUE`, the default, will escape `.` to
 #'   `\\.` and surround with `^` and `$`.
 #' @export
+#' @examples
+#' # ignore all Excel files
+#' use_build_ignore("[.]xls$", escape = FALSE)
+#'
 use_build_ignore <- function(files, escape = TRUE) {
   if (escape) {
     files <- escape_path(files)
