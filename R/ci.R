c9208625df0bdcd9c13f29aaf4e0beae908f5984
John Blischak <jdblischak@gmail.com>
2019-07-21 16:21:44 -0400

    Fix use_circleci (#835)
    * Use alternate mustache delimiters to fix CircleCI cache keys.
    
    https://mustache.github.io/mustache.5.html
    
    * Specifically ignore .circleci directory.
    
    Avoids mess age about empty directory from `R CMD build`:
    
    * checking for empty or unneeded directories
    Removed empty directory ‘usethis/.circleci’
    
    * Make better use of existing helpers and arguments
    
    * Use proj_path()

--- R/ci.R
+++ R/ci.R
@@ -173,7 +173,7 @@ check_uses_gitlab_ci <- function(base_path = proj_get()) {
 #' @rdname ci
 use_circleci <- function(browse = interactive(), image = "rocker/verse:latest") {
   check_uses_github()
-  use_directory(".circleci")
+  use_directory(".circleci", ignore = TRUE)
   new <- use_template(
     "circleci-config.yml",
     ".circleci/config.yml",
