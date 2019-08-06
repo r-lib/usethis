0ee9efcece843f0ce8071a6c68f6054ed5611613
Metin Yazici <stradivariusboul@gmail.com>
2019-08-06 22:37:50 +0200

    Minor correction of a test name in `test-use-r.R`

--- tests/testthat/test-use-r.R
+++ tests/testthat/test-use-r.R
@@ -6,7 +6,7 @@ test_that("use_r() creates a .R file below R/", {
   expect_proj_file("R/foo.R")
 })
 
-test_that("use_r doesn't accept multiple file names", {
+test_that("use_r() doesn't accept multiple file names", {
   pkg <- scoped_temporary_package()
   expect_error(use_r(c("file1", "file2")))
 })
