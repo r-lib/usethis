5e2569be9132c979919ef594f4e380bf60dda1cf
Ali Valcarcel <alval@pennmedicine.upenn.edu>
2019-08-14 19:12:47 -0400

    updated function code based on jennybc comments. tried to remove the conditional if else. had to keep some for checks but got rid of major one.

--- R/authors.R
+++ R/authors.R
@@ -15,7 +15,7 @@
 #' include in `.Rprofile`:
 #'
 #' ``` options( usethis.description = list( `Authors@R` = 'person("Jane", "Doe",
-#' email = "jane@example.com", role = c("aut", "cre"), comment = c(ORCID =
+#' email = "jane@example.com", role = "aut", comment = c(ORCID =
 #' "YOUR-ORCID-ID"))', License = "MIT + file LICENSE", Language =  "es" ) )
 #' ```
 #'
@@ -37,7 +37,7 @@
 #' use_author(
 #'   given = "Ali",
 #'   family = "Val",
-#'   role = c("aut", "cre"),
+#'   role = "aut",
 #'   email = "alval@example.com",
 #'   comment = c(ORCID = "YOUR-ORCID-ID")
 #' )
@@ -46,51 +46,67 @@
 #' use_author(
 #'   given = "Ali2",
 #'   family = "Val2",
-#'   role = c("cph", "fnd"),
+#'   role = "cph",
 #'   email = "alval2@example.com",
 #'   comment = NULL
 #' )
 #' }
-
-
-use_author <- function(given = "Jane", family = "Doe", role = c("aut", "cre"), email = "jane@example.com", comment = c(ORCID = "YOUR-ORCID-ID")) {
+#'
+use_author <- function(given = "Jane", family = "Doe", role = "aut", email = "jane@example.com", comment = c(ORCID = "YOUR-ORCID-ID")) {
   # Adapted from use_dependency code and tools provided in the desc package
+  # TODO figure out how desc package requires input for role with multiple roles
   # TODO long term: create addin to prompt for author information
   # TODO long term: create a snippet with author information for DESCRIPTION
-  # TODO long term: add tests
+  # TODO add tests
 
-  # Set the author as a person
+  # Assume DESCRIPTION is generated from usethis so that Authors@R is filled with either
+  # 1. The temporary author filled in without usethis defaults
+  # person("First", "Last", , "first.last@example.com", c("aut", "cre"), comment = c(ORCID = "YOUR-ORCID-ID"))
+  # 2. True author(s)
+  # TODO long term: make function more robust to fill in author even when usethis did not generate the DESCRIPTION
+
+  fields <- desc::desc_fields(file = proj_get())
+
+  # Check the author field exists
+  if ("Author" %in% fields) {
+    ui_stop(
+      "Author was found as a field value in the DESCRIPTION. \\
+       Please remove or replace it with `Authors@R` before continuing."
+    )
+  }
+
+  # Create person object using inputs
   author <- utils::person(given = given, family = given, role = role, email = email, comment = comment)
 
-  # Obtain the current DESCRIPTION fields
-  current_desc_fields <- desc::desc_fields()
+  # Obtain the current authors in the description
+  desc_authors <- desc::desc_get_authors(file = proj_get())
 
-  # If the `Authors@R` is not a current field then add the field and fill in the author provided
-  if (any(current_desc_fields == "Authors@R") == FALSE) {
-    ui_done("`Authors@R` field added to DESCRIPTION and filled with {ui_value(author)}.")
-    desc::desc_set_authors(
-      authors = author,
-      file = proj_get(),
-      normalize = TRUE
+  # Check if the input author already exists as an author
+  if (any(lapply(desc_authors, identical, author) == TRUE)) {
+    ui_stop(
+      "Author {ui_value(author)} is already listed in \\
+       `Authors@R` in DESCRIPTION, no change made."
     )
-    return(invisible())
-  } else if (any(current_desc_fields == "Authors@R") == TRUE) {
-    # Obtain the current authors in the description
-    desc_authors <- desc::desc_get_authors()
-
-    # Check if any current author in the DESCRIPTION is exactly identical to the author input
-    if (any(lapply(desc_authors, identical, author) == TRUE)) {
-      ui_warn(
-        "Author {ui_value(author)} is already listed in\\
-        `Authors@R` in DESCRIPTION, no change made."
-      )
-    } else {
-      ui_done("`Authors@R` field already exists adding {ui_value(author)} as an additional author.")
-      desc::desc_add_author(given = given, family = given, role = role, email = email, comment = comment, normalize = TRUE)
-      return(invisible())
-    }
-    # TODO check if the author input has partial overlap with one already
-    # listed and ask user if they would like to add the person as a new author
-    # or replace the existing author
   }
+
+  # Check if any current author in the DESCRIPTION is exactly identical to the author input
+  if (any(lapply(desc_authors, identical, author) == TRUE)) {
+    ui_stop(
+      "Author {ui_value(author)} is already listed in\\
+        `Authors@R` in DESCRIPTION, no change made."
+    )
+  }
+
+  # Add the input author
+  desc::desc_add_author(given = given, family = family, role = role, email = email, comment = comment, file = proj_get(), normalize = TRUE)
+  ui_done("`Authors@R` field already exists adding {ui_value(author)} as an additional author.")
+
+  # Check if the usethis default author is included and remove it if so
+  usethis_author <- utils::person("First", "Last", , "first.last@example.com", comment = c(ORCID = "YOUR-ORCID-ID"))
+
+  if (author %in% desc_authors) {
+    desc::desc_del_author(given = "First", family = "Last", email = "first.last@example.com", comment = c(ORCID = "YOUR-ORCID-ID"), file = proj_get())
+  }
+
+  return(invisible())
 }
