f313e4a20f4021e1a739519ca8ab33d0a0614e21
Ali Valcarcel <alval@pennmedicine.upenn.edu>
2019-08-15 19:31:46 -0400

    added tests for use_author. minor tweaks to function use_author while testing. returned description to original. while playing with use_author some stuff got messed up. just copied and pasted from r-lib github.

--- R/authors.R
+++ R/authors.R
@@ -51,10 +51,10 @@
 #'   comment = NULL
 #' )
 #' }
-
+#'
 use_author <- function(given = "Jane", family = "Doe", role = "aut", email = "jane@example.com", comment = c(ORCID = "YOUR-ORCID-ID")) {
   # Adapted from use_dependency code and tools provided in the desc package
-  # TODO figure out how desc package requires input for role with multiple roles
+  # TODO long term: figure out if/how desc package requires input for role with multiple roles (i.e. c("aut", "cre"))
   # TODO long term: create addin to prompt for author information
   # TODO long term: create a snippet with author information for DESCRIPTION
   # TODO add tests
@@ -63,17 +63,23 @@ use_author <- function(given = "Jane", family = "Doe", role = "aut", email = "ja
   # 1. The temporary author filled in without usethis defaults
   # person("First", "Last", , "first.last@example.com", c("aut", "cre"), comment = c(ORCID = "YOUR-ORCID-ID"))
   # 2. True author(s)
-  # TODO long term: make function more robust to fill in author even when usethis did not generate the DESCRIPTION
 
-  fields <- desc::desc_fields()
-
-  # Check the "Author:" field exists
-  if ("Author" %in% fields) {
+  # Check the "Author:" field exists and if it does error the function
+  if (desc::desc_has_fields("Author")) {
     ui_stop(
       "{ui_field('Author')} was found as a field value in the DESCRIPTION. \\
        Please remove or replace it with the {ui_field('Authors@R')} field."
     )
+    # TODO long term: if Author: is found in the description ask user if they
+    # want to remove and replace it with Authors@R using desc::desc_del()
   }
+
+
+  # TODO long term: if Authors@R field is missing from the description ask user
+  # if they want to add a blank one or error our
+  # if(desc::desc_has_fields("Authors@R") == FALSE){
+  #   desc::desc_set(`Authors@R` = '')
+  # }
 
   # Create person object using inputs
   author <- utils::person(given = given, family = family, role = role, email = email, comment = comment)
@@ -96,8 +102,8 @@ use_author <- function(given = "Jane", family = "Doe", role = "aut", email = "ja
   # Check if the usethis default author is included and remove it if so
   usethis_author <- utils::person(given = "First", family = "Last", role = c("aut", "cre"), email = "first.last@example.com", comment = c(ORCID = "YOUR-ORCID-ID"))
 
-  if (usethis_author %in% desc_authors){
-    if(ui_yeah("{ui_field('Authors@R')}` field is populated with the {ui_code('usethis')} \\
+  if (usethis_author %in% desc_authors) {
+    if (ui_yeah("{ui_field('Authors@R')}` field is populated with the {ui_code('usethis')} \\
                default (i.e. {ui_value(usethis_author)}. Would you like to remove the default?")) {
       # TODO(@jennybc): should we suppress messages from the desc::desc_del_author function? If so, how is this handled inside the package? suppressMessages()?
       # Delete the usethis default author (i.e. person(given = "First", family = "Last", email = "first.last@example.com", comment = c(ORCID = "YOUR-ORCID-ID")))
