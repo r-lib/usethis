14dea349f899976d78a8a77c45a80cbb9445742d
Michael Chirico <michael.chirico@grabtaxi.com>
2019-10-22 08:55:34 +0800

    more feedback

--- R/data-table.R
+++ R/data-table.R
@@ -5,23 +5,13 @@
 #'
 #' This function does two main things:
 #'
-#' 1. Import the entire `data.table` namespace (with `@import`); see below.
+#' 1. Import the entire `data.table` namespace (with `@import`).
 #'
 #' 2. Block the usage of `data.table` as a dependency (`DESCRIPTION`
 #' field `Depends`); `data.table` should be used as an _import_ or _suggested_
 #' package only. See this [discussion](https://github.com/Rdatatable/data.table/issues/3076).
-#'
-#' `data.table` is generally careful to minimize the scope for namespace
-#' conflicts (i.e., functions with the same name as in other packages);
-#' a more conservative approach using `@importFrom` should be careful to
-#' import any needed `data.table` special symbols as well, e.g., if you
-#' run `DT[ , .N, by='grp']` in your package, you'll need to add
-#' `@importFrom data.table .N` to prevent the `NOTE` from `R CMD check`.
-#' See \code{?data.table::`special-symbols`} for the list of such symbols
-#' `data.table` defines; see the 'Importing data.table' vignette for more
-#' advice (`vignette('datatable-importing', 'data.table')`).
 #' @export
-use_data_table = function() {
+use_data_table <- function() {
   check_is_package("use_data_table()")
   deps <- desc::desc_get_deps(".")
   if (any(deps$type == 'Depends' & deps$package == "data.table")) {
