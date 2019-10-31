602b8ee41b924421c04a6c4b2abc156d7e2fa440
cervangirard <cervan.girard@gmail.com>
2019-10-31 15:24:11 +0100

    Do not add the Rbuildgnore

--- R/readme.R
+++ R/readme.R
@@ -37,7 +37,7 @@ use_readme_rmd <- function(open = interactive()) {
     if (is_package()) "package-README" else "project-README",
     "README.Rmd",
     data = data,
-    ignore = TRUE,
+    ignore = if(is_package()) TRUE else FALSE,
     open = open
   )
   if (!new) return(invisible(FALSE))
