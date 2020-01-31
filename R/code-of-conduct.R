b752439f59afbed365fe224753d0a31a3522c606
Kiernan Nicholls <kiernann@protonmail.com>
2020-01-31 14:07:02 -0500

    Use 2.0 code of conduct (#989)
    * Use 2.0 code of conduct
    
    The old 1.0 underscore link is dead. It can be accessed using hyphens, but the 2.0 link now works with underscores?
    
    * Update use_code_of_conduct() docs

--- R/code-of-conduct.R
+++ R/code-of-conduct.R
@@ -4,15 +4,15 @@
 #' `.Rbuildignore`, in the case of a package. The goal of a code of conduct is
 #' to foster an environment of inclusiveness, and to explicitly discourage
 #' inappropriate behaviour. The template comes from
-#' <https://contributor-covenant.org>, version 1:
-#' <https://contributor-covenant.org/version/1/0/0>.
+#' <https://contributor-covenant.org>, version 2:
+#' <https://contributor-covenant.org/version/2/0>.
 #'
 #' If your package is going to CRAN, the link to the CoC in your README must
 #' be an absolute link to a rendered website as `CODE_OF_CONDUCT.md` is not
 #' included in the package sent to CRAN. `use_code_of_conduct()` will
 #' automatically generate this link if you use pkgdown and have set the
 #' `url` field in `pkgdown.yml`; otherwise it'll link to
-#' <https://contributor-covenant.org/version/1/0/0>.
+#' <https://contributor-covenant.org/version/2/0>.
 #'
 #' @param path Path of the directory to put `CODE_OF_CONDUCT.md` in, relative to
 #'   the active project. Passed along to [use_directory()]. Default is to locate
@@ -31,7 +31,7 @@ use_code_of_conduct <- function(path = NULL) {
     ignore = is_package() && is.null(path)
   )
 
-  href <- project_pkgdown_url() %||% "https://contributor-covenant.org/version/1/0/0"
+  href <- project_pkgdown_url() %||% "https://contributor-covenant.org/version/2/0"
   href <- paste0(href, "/CODE_OF_CONDUCT.html")
 
   ui_todo("Don't forget to describe the code of conduct in your README:")
