972ab81e865ea2535c54099dcdec5a83e05cc9b8
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-30 10:39:02 -0700

    Work on create_*() tests

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -10,19 +10,19 @@ test_that("create_project() creates a non-package project", {
   expect_false(is_package(dir))
 })
 
-test_that("create functions return path to new proj, but restore active proj", {
+test_that("create_*(open = FALSE) returns path to new proj, restores active proj", {
   path <- file_temp()
-  cur_proj <- proj_get()
+  cur_proj <- proj_get_()
 
-  new_path <- create_package(path)
-  expect_equal(proj_get(), cur_proj)
-  expect_equal(proj_path_prep(path), new_path)
-  dir_delete(path)
+  out_path <- create_package(path, open = FALSE)
+  expect_equal(proj_get_(), cur_proj)
+  expect_equal(proj_path_prep(path), out_path)
+  dir_delete(out_path)
 
-  new_path <- create_project(path)
-  expect_equal(proj_get(), cur_proj)
-  expect_equal(proj_path_prep(path), new_path)
-  dir_delete(path)
+  out_path <- create_project(path, open = FALSE)
+  expect_equal(proj_get_(), cur_proj)
+  expect_equal(proj_path_prep(path), out_path)
+  dir_delete(out_path)
 })
 
 test_that("nested package is disallowed, by default", {
@@ -57,34 +57,45 @@ test_that("nested project can be created if user really, really wants to", {
   expect_false(is_package(child))
 })
 
-test_that("can create package in current directory", {
-  # doing this "by hand" vs. via withr because Windows appears to be unwilling
-  # to delete current working directory
-  newdir <- dir_create(path(file_temp(), "mypackage"))
-  oldwd <- setwd(newdir)
-  on.exit({
-    setwd(oldwd)
-    dir_delete(newdir)
-  })
-  expect_error_free(create_package("."))
+test_that("can create package in current directory (literally in '.')", {
+  target_path <- dir_create(file_temp("mypackage"))
+  withr::defer(dir_delete(target_path))
+  withr::local_dir(target_path)
+  orig_proj <- proj_get_()
+
+  expect_error_free(
+    out_path <- create_package(".", open = FALSE)
+  )
+  expect_equal(path_wd(), out_path)
+  expect_equal(proj_get_(), orig_proj)
 })
 
 ## https://github.com/r-lib/usethis/issues/227
-test_that("create_* works w/ non-existing rel path and absolutizes it", {
-  ## take care to provide a **non-absolute** path
-  path_package <- path_file(file_temp(pattern = "abc"))
-  withr::with_dir(
-    path_temp(),
-    create_package(path_package, rstudio = FALSE, open = FALSE)
-  )
-  expect_true(dir_exists(path_temp(path_package)))
+test_that("create_* works w/ non-existing rel path, open = FALSE case", {
+  sandbox <- path_real(dir_create(file_temp("sandbox")))
+  orig_proj <- proj_get_()
+  orig_wd <- path_wd()
+  withr::defer(dir_delete(sandbox))
+  withr::defer(proj_set(orig_proj, force = TRUE))
+  withr::local_dir(sandbox)
 
-  path_project <- path_file(file_temp(pattern = "abc"))
-  withr::with_dir(
-    path_temp(),
-    create_project(path_project, rstudio = FALSE, open = FALSE)
+  rel_path_pkg <- path_file(file_temp(pattern = "abc"))
+  expect_error_free(
+    out_path <- create_package(rel_path_pkg, open = FALSE)
   )
-  expect_true(dir_exists(path_temp(path_project)))
+  expect_true(dir_exists(rel_path_pkg))
+  expect_equal(out_path, proj_path_prep(rel_path_pkg))
+  expect_equal(proj_get_(), orig_proj)
+  expect_equal(path_wd(), sandbox)
+
+  rel_path_proj <- path_file(file_temp(pattern = "def"))
+  expect_error_free(
+    out_path <- create_project(rel_path_proj, open = FALSE)
+  )
+  expect_true(dir_exists(rel_path_proj))
+  expect_equal(out_path, proj_path_prep(rel_path_proj))
+  expect_equal(proj_get_(), orig_proj)
+  expect_equal(path_wd(), sandbox)
 })
 
 test_that("rationalize_fork() honors fork = FALSE", {
