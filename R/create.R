3ed516c606c63aa9f691b160d05c876e1eb4efc9
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-04-30 11:01:21 -0700

    Activate the project, as presented by proj_get()
    Fixes #1122
    
    Now we send proj_activate() an absolutized, realized path, which leaves much less room for headache if input was a relative path.

--- R/create.R
+++ R/create.R
@@ -59,8 +59,8 @@ create_package <- function(path,
   }
 
   if (open) {
-    if (proj_activate(path)) {
-      # Working directory/active project changed; so don't undo on exit
+    if (proj_activate(proj_get())) {
+      # working directory/active project already set; clear the on.exit()'s
       on.exit()
     }
   }
@@ -93,8 +93,8 @@ create_project <- function(path,
   }
 
   if (open) {
-    if (proj_activate(path)) {
-      # Working directory/active project changed; so don't undo on exit
+    if (proj_activate(proj_get())) {
+      # working directory/active project already set; clear the on.exit()'s
       on.exit()
     }
   }
