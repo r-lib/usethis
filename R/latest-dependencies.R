0144ea1044797604ca97997c7f4be964e2fec30c
Salim B <salim@posteo.de>
2020-05-14 00:04:42 +0200

    Fix description (#1110)
    * Fix description
    
    * Fix tidyverse mistake

--- R/latest-dependencies.R
+++ R/latest-dependencies.R
@@ -1,8 +1,7 @@
 #' Use "latest" versions of all dependencies
 #'
-#' Pins minimum version of dependencies to latest (as determined by "source)"
-#' Useful for tidyverse package, but should otherwise be used with extreme
-#' care.
+#' Pins minimum versions of dependencies to latest ones (as determined by `source`).
+#' Useful for the tidyverse package, but should otherwise be used with extreme care.
 #'
 #' @keywords internal
 #' @export
