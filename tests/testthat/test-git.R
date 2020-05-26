7d904af59ec300a03e89a755acb59b8b333228c6
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-26 11:56:49 -0700

    Add tests for uses_git(), check_uses_git()

--- tests/testthat/test-git.R
+++ tests/testthat/test-git.R
@@ -1,10 +1,22 @@
+test_that("uses_git() works", {
+  skip_if_no_git_user()
+
+  create_local_package()
+  expect_false(uses_git())
+  expect_error(check_uses_git())
+
+  git_init()
+
+  expect_true(uses_git())
+  expect_error_free(check_uses_git())
+})
+
 test_that("git_has_commit() changes after first commit", {
   skip_if_no_git_user()
 
   create_local_package()
   git_init()
 
-  expect_true(uses_git())
   expect_false(git_has_commits())
 
   git_commit("DESCRIPTION", "test")
