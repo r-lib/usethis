be21e412498a6a47f6638a636d5eacf9e3531dd0
Hadley Wickham <h.wickham@gmail.com>
2020-05-28 14:17:37 -0500

    Don't remind to check pkgdown index for patch release (#1141)

--- R/release.R
+++ R/release.R
@@ -66,7 +66,7 @@ release_checklist <- function(version, on_cran) {
     todo("`revdepcheck::revdep_check(num_workers = 4)`", on_cran),
     todo("Update `cran-comments.md`"),
     todo("[Polish NEWS](https://style.tidyverse.org/news.html#news-release)", on_cran),
-    todo("Review pkgdown reference index for, e.g., missing topics", has_pkgdown),
+    todo("Review pkgdown reference index for, e.g., missing topics", has_pkgdown && type != "patch"),
     todo("Draft blog post", type != "patch"),
     if (has_extra) paste0("* [ ] ", get("release_bullets", parent.env(globalenv()))()),
     "",
