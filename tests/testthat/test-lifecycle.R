d14be5fe443b510b9bb7aca17aaf8f6eaf8931e3
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-06-11 09:01:56 -0700

    Refactor tests re: use_lifecycle()
    Apply the mock at the exact point of need

--- tests/testthat/test-lifecycle.R
+++ tests/testthat/test-lifecycle.R
@@ -1,30 +1,35 @@
-context("lifecycle")
-
-with_mock(`usethis:::is_installed` = function(pkg) TRUE, {
-  test_that("use_lifecycle() imports badges", {
-    create_local_package()
+test_that("use_lifecycle() imports badges", {
+  create_local_package()
+  with_mock(
+    `usethis:::is_installed` = function(pkg) TRUE,
     use_lifecycle()
-    expect_proj_file("man", "figures", "lifecycle-stable.svg")
+  )
+  expect_proj_file("man", "figures", "lifecycle-stable.svg")
 
-    # Idempotent
-    expect_error_free(use_lifecycle())
-  })
+  # Idempotent
+  expect_error_free(use_lifecycle())
+})
 
-  test_that("use_lifecycle() adds RdMacros field", {
-    create_local_package()
+test_that("use_lifecycle() adds RdMacros field", {
+  create_local_package()
+  with_mock(
+    `usethis:::is_installed` = function(pkg) TRUE,
     use_lifecycle()
+  )
 
-    expect_true(desc::desc_has_fields("RdMacros"))
-    expect_identical(desc::desc_get_field("RdMacros"), "lifecycle")
-  })
+  expect_true(desc::desc_has_fields("RdMacros"))
+  expect_identical(desc::desc_get_field("RdMacros"), "lifecycle")
+})
 
-  test_that("use_lifecycle() respects existing RdMacros field", {
-    create_local_package()
+test_that("use_lifecycle() respects existing RdMacros field", {
+  create_local_package()
 
-    desc::desc_set(RdMacros = "foo, bar")
+  desc::desc_set(RdMacros = "foo, bar")
+  with_mock(
+    `usethis:::is_installed` = function(pkg) TRUE,
     use_lifecycle()
+  )
 
-    expect_true(desc::desc_has_fields("RdMacros"))
-    expect_identical(desc::desc_get_field("RdMacros"), "foo, bar, lifecycle")
-  })
+  expect_true(desc::desc_has_fields("RdMacros"))
+  expect_identical(desc::desc_get_field("RdMacros"), "foo, bar, lifecycle")
 })
