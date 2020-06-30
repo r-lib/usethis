84c85987412e7cd170f04739bf2eccbc5564d853
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-06-30 09:40:08 -0700

    Use mock so test runs even if cpp11 not installed

--- tests/testthat/test-cpp11.R
+++ tests/testthat/test-cpp11.R
@@ -7,7 +7,12 @@ test_that("use_cpp11() creates files/dirs, edits DESCRIPTION and .gitignore", {
   pkg <- create_local_package()
   use_roxygen_md()
 
-  use_cpp11()
+  with_mock(
+    # Required to pass the check re: whether cpp11 is installed
+    `usethis:::is_installed` = function(pkg) TRUE,
+    use_cpp11()
+  )
+
   expect_match(desc::desc_get("LinkingTo", pkg), "cpp11")
   expect_proj_dir("src")
 
