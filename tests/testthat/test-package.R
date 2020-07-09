2f7baa9c08f63ddf3c993b71ebb090ea3919a153
Julia Silge <julia.silge@gmail.com>
2020-07-09 14:37:38 -0600

    tidymodels is *also* verboten

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -1,6 +1,7 @@
-test_that("use_package() won't facilitate dependency on tidyverse", {
+test_that("use_package() won't facilitate dependency on tidyverse/tidymodels", {
   create_local_package()
   expect_usethis_error(use_package("tidyverse"), "rarely a good idea")
+  expect_usethis_error(use_package("tidymodels"), "rarely a good idea")
 })
 
 
