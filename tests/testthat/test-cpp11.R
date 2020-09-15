905c83a264571220b57a932e400e714f657853b0
Jim Hester <james.f.hester@gmail.com>
2020-09-15 09:29:25 -0400

    Add tests for check_cpp_register_deps, mock desc call

--- tests/testthat/test-cpp11.R
+++ tests/testthat/test-cpp11.R
@@ -10,6 +10,7 @@ test_that("use_cpp11() creates files/dirs, edits DESCRIPTION and .gitignore", {
   with_mock(
     # Required to pass the check re: whether cpp11 is installed
     `usethis:::is_installed` = function(pkg) TRUE,
+    `usethis:::check_cpp_register_deps` = function() invisible(),
     use_cpp11()
   )
 
@@ -18,4 +19,25 @@ test_that("use_cpp11() creates files/dirs, edits DESCRIPTION and .gitignore", {
 
   ignores <- read_utf8(proj_path("src", ".gitignore"))
   expect_true(all(c("*.o", "*.so", "*.dll") %in% ignores))
+})
+
+test_that("check_cpp_register_deps is silent if all installed, emits todo if not", {
+  withr::local_options(list(usethis.quiet = FALSE))
+
+  with_mock(
+    `usethis:::get_cpp_register_deps` = function() c("brio", "decor", "vctrs"),
+    `usethis:::is_installed` = function(pkg) TRUE,
+    expect_silent(
+      check_cpp_register_deps()
+    )
+  )
+
+  with_mock(
+    `usethis:::get_cpp_register_deps` = function() c("brio", "decor", "vctrs"),
+    `usethis:::is_installed` = function(pkg) pkg == "brio",
+    expect_message(
+      check_cpp_register_deps(),
+      "Now install"
+    )
+  )
 })
