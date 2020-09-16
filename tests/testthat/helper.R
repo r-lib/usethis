66889463fc31a938e751c1e470fed51c3fa604f6
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-09-15 17:32:47 -0700

    Refactor create_local_thing()
    Main change is to NOT assume that original working directory is same as original project. The original project could be `NULL`. This surfaced because of changes coming in withr.
    
    While I was refactoring, I also refactored to be more in line with the recommended style in the new testthat "test fixtures" vignette.

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -37,26 +37,24 @@ create_local_thing <- function(dir = file_temp(pattern = pattern),
     ui_stop("Target {ui_code('dir')} {ui_path(dir)} already exists.")
   }
 
-  old_project <- proj_get_()
-  withr::defer(
-    {
-      ui_silence({
-        proj_set(old_project, force = TRUE)
-      })
-      setwd(old_project)
-      fs::dir_delete(dir)
-    },
-    envir = env
-  )
+  old_project <- proj_get_() # this could be `NULL`, i.e. no active project
+  old_wd <- getwd()          # not necessarily same as `old_project`
 
-  ui_silence({
-    switch(thing,
+  withr::defer(fs::dir_delete(dir), envir = env)
+  ui_silence(
+    switch(
+      thing,
       package = create_package(dir, rstudio = rstudio, open = FALSE, check_name = FALSE),
       project = create_project(dir, rstudio = rstudio, open = FALSE)
     )
-    proj_set(dir)
-  })
+  )
+
+  withr::defer(setwd(old_wd), envir = env)
   setwd(dir)
+
+  withr::defer(ui_silence(proj_set(old_project, force = TRUE)), envir = env)
+  proj_set(dir)
+
   invisible(dir)
 }
 
