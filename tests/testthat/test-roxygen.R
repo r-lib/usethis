b993e839d9e90c925aaeb7c9f6f603ca001f44f6
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-09-29 16:00:02 -0700

    Rename files to align with current filename conventions
    Makes use_r() / use_test(), test_file(), etc. work better

--- tests/testthat/test-roxygen.R
+++ tests/testthat/test-roxygen.R
@@ -6,3 +6,15 @@ test_that("use_package_doc() compatible with roxygen_ns_append()", {
   expect_message(roxygen_ns_append("test"), "Adding 'test'")
   expect_silent(roxygen_ns_append("test"))
 })
+
+test_that("use_roxygen_md() adds DESCRIPTION fields to naive package", {
+  skip_if_not_installed("roxygen2")
+  pkg <- create_local_package()
+  use_roxygen_md()
+  expect_identical(
+    desc::desc_get("Roxygen", pkg),
+    c(Roxygen = "list(markdown = TRUE)")
+  )
+  expect_true(desc::desc_has_fields("RoxygenNote", pkg))
+  expect_true(uses_roxygen_md())
+})
