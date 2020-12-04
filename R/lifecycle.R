36323457e435ff303e83505b89db79346ed6c444
Hadley Wickham <h.wickham@gmail.com>
2020-12-04 10:27:26 -0600

    Remove badges we rarely use for functions (#1303)

--- R/lifecycle.R
+++ R/lifecycle.R
@@ -34,13 +34,9 @@ use_lifecycle <- function() {
   ui_todo(c(
     "Add badges in documentation topics by inserting one of:",
     "- `r lifecycle::badge('experimental')`",
-    "- `r lifecycle::badge('maturing')`",
-    "- `r lifecycle::badge('stable')`",
     "- `r lifecycle::badge('superseded')`",
     "- `r lifecycle::badge('questioning')`",
-    "- `r lifecycle::badge('deprecated')`",
-    "- `r lifecycle::badge('defunct')`",
-    "- `r lifecycle::badge('archived')`"
+    "- `r lifecycle::badge('deprecated')`"
   ))
 
   ui_todo(c(
