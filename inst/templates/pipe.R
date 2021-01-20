5f32e4b7aa0cb0ff12bf5308a9c221a837043529
Carson <cpsievert1@gmail.com>
2021-01-20 09:20:28 -0600

    Closes #1333: use_pipe() now documents arguments and return value (to help avoid manual inspection road-blocks on CRAN)

--- inst/templates/pipe.R
+++ inst/templates/pipe.R
@@ -8,4 +8,7 @@
 #' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @returns the result of calling `rhs(lhs)`.
 NULL
