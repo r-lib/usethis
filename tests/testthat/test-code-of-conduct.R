c6d44f6432d164165641559a1ae0430c2b3f0176
Hadley Wickham <h.wickham@gmail.com>
2021-02-25 08:04:40 -0600

    Minor fixes from review

--- tests/testthat/test-code-of-conduct.R
+++ tests/testthat/test-code-of-conduct.R
@@ -1,5 +1,5 @@
 test_that("use_code_of_conduct() creates promised file", {
   create_local_project()
-  use_code_of_conduct("test@gmail.com")
+  use_code_of_conduct("test@example.com")
   expect_proj_file("CODE_OF_CONDUCT.md")
 })
