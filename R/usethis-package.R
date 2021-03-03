8e8cde42934ff00098a499f4ee6fa34d7154f8dc
Hadley Wickham <h.wickham@gmail.com>
2021-03-03 07:09:33 -0600

    Drop warning

--- R/usethis-package.R
+++ R/usethis-package.R
@@ -5,8 +5,6 @@
 #' @import rlang
 "_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
