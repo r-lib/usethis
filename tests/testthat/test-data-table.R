26434c1585122cae9d229879af93b7f6f4e7b0eb
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-03-17 19:00:30 -0700

    just don't test when data.table not installed

--- tests/testthat/test-data-table.R
+++ tests/testthat/test-data-table.R
@@ -1,3 +1,5 @@
+skip_if_not_installed("data.table")
+
 test_that("use_data_table() requires a package", {
   create_local_project()
   expect_usethis_error(use_data_table(), "not an R package")
@@ -8,7 +10,6 @@ test_that("use_data_table() Imports data.table", {
   use_package_doc()
   with_mock(
     check_installed = function(pkg) TRUE,
-    check_fun_exists = function(...) TRUE,
     use_data_table()
   )
   expect_match(desc::desc_get("Imports"), "data.table")
@@ -30,7 +31,6 @@ test_that("use_data_table() blocks use of Depends", {
   desc::desc_set_dep("data.table", "Depends")
   with_mock(
     check_installed = function(pkg) TRUE,
-    check_fun_exists = function(...) TRUE,
     expect_warning(
       use_data_table(),
       "data.table should be in Imports or Suggests, not Depends"
