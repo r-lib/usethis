25adfe1d6653772038e99e961aa3e96355e22416
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-10-13 21:32:21 -0700

    Move some @importFrom's

--- R/usethis-package.R
+++ R/usethis-package.R
@@ -1,12 +1,11 @@
 #' @keywords internal
-#' @importFrom glue glue_collapse glue_data
-#' @importFrom purrr map map_chr map_lgl map_int
 #' @import fs
 #' @import rlang
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom glue glue
+#' @importFrom glue glue glue_collapse glue_data
 #' @importFrom lifecycle deprecated
+#' @importFrom purrr map map_chr map_lgl map_int
 ## usethis namespace: end
 NULL
