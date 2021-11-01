695664ae151bd6b5678797f61dc3fa02531dd88d
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-11-01 15:56:38 -0700

    Don't use ui_*() functions inside ui_code_block()

--- R/logo.R
+++ R/logo.R
@@ -51,8 +51,8 @@ use_logo <- function(img, geometry = "240x278", retina = TRUE) {
   ui_todo("Add logo to your README with the following html:")
   pd_link <- pkgdown_url(pedantic = TRUE)
   if (is.null(pd_link)) {
-    ui_code_block("# {pkg} <img src={ui_path(logo_path)} align=\"right\" height=\"{height}\" />")
+    ui_code_block("# {pkg} <img src=\"{proj_rel_path(logo_path)}\" align=\"right\" height=\"{height}\" />")
   } else {
-    ui_code_block("# {pkg} <a href={ui_value(pd_link)}><img src={ui_path(logo_path)} align=\"right\" height=\"{height}\" /></a>")
+    ui_code_block("# {pkg} <a href=\"{pd_link}\"><img src=\"{proj_rel_path(logo_path)}\" align=\"right\" height=\"{height}\" /></a>")
   }
 }
