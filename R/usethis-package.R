2402ea8e4eb098b5fd18770174fa86ae1a803658
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-03-01 08:32:50 -0800

    Add a release bullet re: CoC
    Closes #1484

--- R/usethis-package.R
+++ R/usethis-package.R
@@ -9,3 +9,9 @@
 #' @importFrom purrr map map_chr map_lgl map_int
 ## usethis namespace: end
 NULL
+
+release_bullets <- function() {
+  c(
+    "Check that `use_code_of_conduct()` is shipping the latest version of the Contributor Covenant (<https://www.contributor-covenant.org>)."
+  )
+}
