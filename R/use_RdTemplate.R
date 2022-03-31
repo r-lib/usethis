a7807b8e09b54d4d78cdc6282a3be2df94921d02
mikeroswell <mike.roswell@gmail.com>
2022-03-31 12:22:15 -0400

    potential new feature for adding "templates" for documentation to R packages.

---
+++ R/use_RdTemplate.R
@@ -0,0 +1,19 @@
+#' Create a template for package documentation
+#'
+#' @param templateName Character, name of template and file
+#'
+#' @return This function creates a `.R` file in the directory "man-roxygen". If
+#' that directory does not already exist in your package, it will create it.
+#' The function opens the new (blank) file in your editor.
+#'
+#' @seealso \code{\link{file.edit}}
+#' @export
+#'
+use_RdTemplate <- function(templateName){
+  if(!dir.exists("man-roxygen")){
+    dir.create("man-roxygen")
+  }
+  pth <- paste0("man-roxygen/", templateName, ".R")
+  file.create(pth)
+  file.edit(pth)
+}
