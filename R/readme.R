95cfc96c86fa48b43cce86d73c04982bdf12a38c
TJ Mahr <tjmahr@users.noreply.github.com>
2022-05-19 14:59:29 -0500

    add note to use_readme_rmd() docs (#1620)
    * add note to use_readme_rmd docs
    
    * Work on wording
    
    Co-authored-by: Jenny Bryan <jenny.f.bryan@gmail.com>

--- R/readme.R
+++ R/readme.R
@@ -1,8 +1,8 @@
 #' Create README files
 #'
 #' @description
-#' Creates skeleton README files with sections for
-#' * a high-level description of the package and its goals
+#' Creates skeleton README files with possible stubs for
+#' * a high-level description of the project/package and its goals
 #' * R code to install from GitHub, if GitHub usage detected
 #' * a basic example
 #'
@@ -16,6 +16,13 @@
 #' could also use GitHub Actions to re-render `README.Rmd` every time you push.
 #' An example workflow can be found in the `examples/` directory here:
 #' <https://github.com/r-lib/actions/>.
+#'
+#' If the current project is a Git repo, then `use_readme_rmd()` automatically
+#' configures a pre-commit hook that helps keep `README.Rmd` and `README.md`,
+#' synchronized. The hook creates friction if you try to commit when
+#' `README.Rmd` has been edited more recently than `README.md`. If this hook
+#' causes more problems than it solves for you, it is implemented in
+#' `.git/hooks/pre-commit`, which you can modify or even delete.
 #'
 #' @inheritParams use_template
 #' @seealso The [important files
