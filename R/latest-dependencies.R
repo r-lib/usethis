ff93aa614f4339027c3f815a5d1ef8aa08f3f7b9
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-08-29 18:57:44 -0700

    Make order in docs match default seen in signature

--- R/latest-dependencies.R
+++ R/latest-dependencies.R
@@ -8,7 +8,7 @@
 #' @param overwrite By default (`TRUE`), all dependencies will be modified.
 #'   Set to `FALSE` to only modify dependencies without version
 #'   specifications.
-#' @param source Use "local" or "CRAN" package versions.
+#' @param source Use "CRAN" or "local" package versions.
 use_latest_dependencies <- function(overwrite = TRUE, source = c("CRAN"," local")) {
   deps <- desc::desc_get_deps(proj_get())
   source <- arg_match(source)
