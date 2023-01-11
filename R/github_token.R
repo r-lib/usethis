3e930fa0b95d30e6ef79fb2a8ea0df0910435af5
Ian Lyttle <ijlyttle@me.com>
2023-01-11 14:06:14 -0600

    handle case where user-email not set

--- R/github_token.R
+++ R/github_token.R
@@ -181,6 +181,11 @@ pat_sitrep <- function(host = "https://github.com",
     ui_silence(
       de_facto_email <- git_cfg_get("user.email", "de_facto")
     )
+    if (is.null(de_facto_email)) {
+      hint <- "use_git_config(user.email = \"<your email>\")"
+      ui_oops("Git user's email is not set. Configure using {ui_code(hint)}.")
+      return(invisible(FALSE))
+    }
     if (!any(grepl(de_facto_email, addresses))) {
       ui_oops("
         Local Git user's email ({ui_value(de_facto_email)}) doesn't appear to \\
