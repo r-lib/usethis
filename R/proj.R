e770909707e9854073c50d5d8488aa87f06d308b
Hadley Wickham <h.wickham@gmail.com>
2023-01-21 09:23:16 -0600

    Move desc stuff to own file

--- R/proj.R
+++ R/proj.R
@@ -279,15 +279,3 @@ proj_activate <- function(path) {
     invisible(TRUE)
   }
 }
-
-proj_desc <- function(path = proj_get()) {
-  desc::desc(file = path)
-}
-
-proj_version <- function() {
-  proj_desc()$get_field("Version")
-}
-
-proj_deps <- function() {
-  proj_desc()$get_deps()
-}
