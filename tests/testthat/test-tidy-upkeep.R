7f16ed2842912a8fe496b4fbf3276706141159bd
Andy Teucher <andy.teucher@gmail.com>
2023-03-08 16:05:44 -0800

    create_local_package() in test-tidy-upkeep
    The use of desc() inside upkeep_checklist() requires a package structure.

--- tests/testthat/test-tidy-upkeep.R
+++ tests/testthat/test-tidy-upkeep.R
@@ -1,4 +1,5 @@
 test_that("upkeep bullets don't change accidentally", {
+  create_local_package()
   expect_snapshot(writeLines(
     upkeep_checklist(posit_pkg = TRUE, posit_person_ok = FALSE)
   ))
