6c3cb6cd50dc71ae7d2367f8a943e19b9dbb6219
Andy Teucher <andy.teucher@gmail.com>
2023-04-27 13:07:45 -0700

    Update tests, lean more on desc

---
+++ tests/testthat/_snaps/github.md
@@ -0,0 +1,8 @@
+# use_github_links errors when overwrite = FALSE
+
+    Code
+      use_github_links(overwrite = FALSE)
+    Condition
+      Error:
+      ! URL has a different value in DESCRIPTION. Use `overwrite = TRUE` to overwrite.
+
--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -1,45 +1,67 @@
 test_that("use_github_links populates empty URL field", {
+  local_interactive(FALSE)
   create_local_package()
   use_git()
+  skip_if_no_git_user()
+
   local_mocked_bindings(
     gh_tr = function(tr) {
-        function(endpoint, ...) list(html_url = "https://github.com/USER/REPO")
+        function(endpoint, ...) list(html_url = "https://github.com/OWNER/REPO")
       },
     target_repo = function(github_get) NULL
   )
 
   # when no URL field
   use_github_links()
-  expect_equal(proj_desc()$get_urls(), "https://github.com/USER/REPO")
+  expect_equal(proj_desc()$get_urls(), "https://github.com/OWNER/REPO")
+  expect_equal(
+    proj_desc()$get_field("BugReports"),
+    "https://github.com/OWNER/REPO/issues"
+    )
 })
 
-test_that("use_github_links overwrites field when overwrite = TRUE", {
+test_that("use_github_links errors when overwrite = FALSE and existing urls", {
+  local_interactive(FALSE)
   create_local_package()
+
   use_git()
+  skip_if_no_git_user()
   local_mocked_bindings(
     gh_tr = function(tr) {
-        function(endpoint, ...) list(html_url = "https://github.com/USER/REPO")
+      function(endpoint, ...) list(html_url = "https://github.com/OWNER/REPO")
+    },
+    target_repo = function(github_get) NULL
+  )
+
+  d <- proj_desc()
+  d$set_urls("https://existing.url")
+  d$write()
+
+  expect_snapshot(use_github_links(overwrite = FALSE), error = TRUE)
+})
+
+test_that("use_github_links appends to URL field when overwrite = TRUE", {
+  local_interactive(FALSE)
+  create_local_package()
+  use_git()
+  skip_if_no_git_user()
+
+  local_mocked_bindings(
+    gh_tr = function(tr) {
+        function(endpoint, ...) list(html_url = "https://github.com/OWNER/REPO")
       },
     target_repo = function(github_get) NULL
   )
-  # when an existing url, and overwrite = TRUE, should overwrite with repo URL
-  use_description_field("URL", "https://existing.url", overwrite = TRUE)
+
+  # when an existing field, and overwrite = TRUE, should append
+  d <- proj_desc()
+  d$set_urls(c("https://existing.url", "https://existing.url1"))
+  d$write()
+
   use_github_links(overwrite = TRUE)
-  expect_equal(proj_desc()$get_urls(), "https://github.com/USER/REPO")
-})
-
-test_that("use_github_links appends to URL field when overwrite = FALSE", {
-  create_local_package()
-  use_git()
-  local_mocked_bindings(
-    gh_tr = function(tr) {
-        function(endpoint, ...) list(html_url = "https://github.com/USER/REPO")
-      },
-    target_repo = function(github_get) NULL
+  expect_equal(
+    proj_desc()$get_urls(),
+    c("https://existing.url", "https://existing.url1",
+      "https://github.com/OWNER/REPO")
   )
-  # when an existing field, and overwrite = FALSE, should append
-  use_description_field("URL", "https://existing.url", overwrite = TRUE)
-  use_github_links(overwrite = FALSE)
-  expect_equal(proj_desc()$get_urls(),
-               c("https://existing.url", "https://github.com/USER/REPO"))
 })
