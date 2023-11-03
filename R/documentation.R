6944309f765a50349470b8fcb032483fdbfc3e48
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-11-03 12:40:09 -0700

    Refinements

--- R/documentation.R
+++ R/documentation.R
@@ -1,28 +1,30 @@
 #' Package-level documentation
 #'
-#' Adds a dummy `.R` file that will prompt roxygen to generate basic
+#' Adds a dummy `.R` file that will cause roxygen2 to generate basic
 #' package-level documentation. If your package is named "foo", this will make
 #' help available to the user via `?foo` or `package?foo`. Once you call
-#' `devtools::document()`, roxygen will flesh out the `.Rd` file using data from
-#' the `DESCRIPTION`. That ensures you don't need to repeat the same information
-#' in multiple places. This `.R` file is also a good place for roxygen
-#' directives that apply to the whole package (vs. a specific function), such as
-#' global namespace tags like `@importFrom`.
+#' `devtools::document()`, roxygen2 will flesh out the `.Rd` file using data
+#' from the `DESCRIPTION`. That ensures you don't need to repeat (and remember
+#' to update!) the same information in multiple places. This `.R` file is also a
+#' good place for roxygen directives that apply to the whole package (vs. a
+#' specific function), such as global namespace tags like `@importFrom`.
 #'
+#' There is one tricky case: when a **package** named "foo" also has a
+#' **function** named `foo()`. In this case, after running `use_package_doc()`,
+#' there will be an `R CMD check` warning. The solution is to set the `@aliases`
+#' for the package-level help topic by modifying the dummy `.R` file (e.g.,
+#' `foo-package.R`) as shown below (but substitute the name of your package /
+#' function for "foo"):
+#'
+#' ```
+#' #' @keywords internal
+#' #' @aliases foo-package
+#' #' "_PACKAGE"
+#' ```
 #' @seealso The [documentation chapter](https://r-pkgs.org/man.html) of [R
 #'   Packages](https://r-pkgs.org)
 #' @inheritParams use_template
 #' @export
-#' @details
-#' use_package_doc() leads to an R-CMD-CHECK warning if package shares name with a
-#' function ([Github Issue](https://github.com/r-lib/usethis/issues/1170)). This can be
-#' obviated by including the following lines at the top of your mypackagename-package.R script:
-#'
-#' `@keywords internal`
-#'
-#' `@aliases reprex-package`
-#'
-#' `"_PACKAGE"`
 use_package_doc <- function(open = rlang::is_interactive()) {
   check_is_package("use_package_doc()")
   use_template(
