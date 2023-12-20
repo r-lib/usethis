2466cc85b8d53c54d891deb50e34cc93c317e83a
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-12-19 16:12:31 -0800

    Add a test of usethis.quiet

--- tests/testthat/test-ui.R
+++ tests/testthat/test-ui.R
@@ -13,6 +13,19 @@ test_that("basic UI actions behave as expected", {
   })
 })
 
+test_that("UI actions respect usethis.quiet = TRUE", {
+  withr::local_options(list(usethis.quiet = TRUE))
+
+  expect_snapshot({
+    ui_line("line")
+    ui_todo("to do")
+    ui_done("done")
+    ui_oops("oops")
+    ui_info("info")
+    ui_code_block(c("x <- 1", "y <- 2"))
+  })
+})
+
 test_that("ui_stop() works", {
   expect_usethis_error(ui_stop("an error"), "an error")
 })
