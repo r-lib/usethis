269d2a2763302adcccf4e773ba6fba66266d1d0e
Olivier Roy <olivierroy71@hotmail.com>
2024-03-06 08:51:37 -0500

    Make `check_is_package()` give a help link

--- R/proj.R
+++ R/proj.R
@@ -214,8 +214,10 @@ check_is_package <- function(whos_asking = NULL) {
 
   message <- "Project {.val {project_name()}} is not an R package."
   if (!is.null(whos_asking)) {
+    # remove brackets, so that it is recognized as a help link
+    whos_asking_fn <- sub("\\(\\)", "", whos_asking)
     message <- c(
-      "i" = "{.code {whos_asking}} is designed to work with packages.",
+      "i" = "{.topic [{whos_asking}](usethis::{whos_asking_fn})} is designed to work with packages.",
       "x" = message
     )
   }
