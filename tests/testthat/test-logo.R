0beb17a559025b6be64ed263dd21e30b2fa1077b
Olivier Roy <olivierroy71@hotmail.com>
2024-03-06 09:27:30 -0500

    Add README link in `use_logo()`.

--- tests/testthat/test-logo.R
+++ tests/testthat/test-logo.R
@@ -1,8 +1,19 @@
-test_that("use_logo() doesn't error", {
+test_that("use_logo() doesn't error with no README", {
   skip_if_not_installed("magick")
   skip_on_os("solaris")
 
   create_local_package()
   img <- magick::image_write(magick::image_read("logo:"), "logo.png")
   expect_no_error(use_logo("logo.png"))
+})
+
+test_that("use_logo() shows a clickable path with README", {
+  skip_if_not_installed("magick")
+  skip_on_os("solaris")
+
+  create_local_package()
+  use_readme_md()
+  img <- magick::image_write(magick::image_read("logo:"), "logo.png")
+  withr::local_options(usethis.quiet = FALSE)
+  expect_snapshot(use_logo("logo.png"),  transform = scrub_testpkg)
 })
