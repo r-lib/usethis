4ce704bf2cb35b873c673a4673d99a97c1e120b4
laurabrianna <lauragast@gmail.com>
2024-08-15 15:10:53 -0700

    Bump default serialization version in use_data() to 3 (#2044)
    * Fixes #1966 - bumped default serialization version in use_data() to 3
    
    * addition of bullet in NEWS.md to alert for update to use_data() version bump
    
    * update param in use_data & related test for default version 3

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,6 @@
 # usethis (development version)
 
+* `use_data()` now uses serialization version 3 by default. (@laurabrianna, #2044)
 * Reverse dependency checks are only suggested if they exist 
   (#1817, @seankross).
 
--- R/data.R
+++ R/data.R
@@ -18,9 +18,9 @@
 #'   files. If you really want to do so, set this to `TRUE`.
 #' @param compress Choose the type of compression used by [save()].
 #'   Should be one of "gzip", "bzip2", or "xz".
-#' @param version The serialization format version to use. The default, 2, was
-#'   the default format from R 1.4.0 to 3.5.3. Version 3 became the default from
-#'   R 3.6.0 and can only be read by R versions 3.5.0 and higher.
+#' @param version The serialization format version to use. The default, 3, can
+#'   only be read by R versions 3.5.0 and higher. For R 1.4.0 to 3.5.3, use
+#'   version 2.
 #' @inheritParams base::save
 #'
 #' @seealso The [data chapter](https://r-pkgs.org/data.html) of [R
@@ -38,7 +38,7 @@ use_data <- function(...,
                      internal = FALSE,
                      overwrite = FALSE,
                      compress = "bzip2",
-                     version = 2,
+                     version = 3,
                      ascii = FALSE) {
   check_is_package("use_data()")
 
