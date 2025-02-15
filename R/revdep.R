6bc8e1ec46b021cc44dbc6da657c7eeb18d595cd
Salim B <git@salim.space>
2025-02-15 05:27:49 +0100

    docs: link to revdepcheck fn
    plus minor tweaks

--- R/revdep.R
+++ R/revdep.R
@@ -1,11 +1,11 @@
 #' Reverse dependency checks
 #'
-#' Performs set up for checking the reverse dependencies of an R package, as
+#' Performs setup for checking the reverse dependencies of an R package, as
 #' implemented by the revdepcheck package:
-#' * Creates `revdep/` directory and adds it to `.Rbuildignore`
+#' * Creates `revdep/` directory and adds it to `.Rbuildignore`.
 #' * Populates `revdep/.gitignore` to prevent tracking of various revdep
-#' artefacts
-#' * Prompts user to run the checks with `revdepcheck::revdep_check()`
+#' artefacts.
+#' * Prompts the user to run the checks with [revdepcheck::revdep_check()].
 #'
 #' @export
 use_revdep <- function() {
