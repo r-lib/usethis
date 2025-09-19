f5e038bdb2986bf4928e2bfab76a620de20d067f
Kara Woo <kara.woo@posit.co>
2025-09-19 15:53:59 -0400

    add "mockable"

--- inst/templates/packagename-package.R
+++ inst/templates/packagename-package.R
@@ -4,6 +4,6 @@
 ## usethis namespace: start
 ## usethis namespace: end
 
-## usethis bindings: start
-## usethis bindings: end
+## usethis mockable bindings: start
+## usethis mockable bindings: end
 NULL
