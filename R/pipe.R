33f272819360eabe8a59865625bd1fc0d2982d97
math-mcshane <math.mcshane@gmail.com>
2025-09-19 14:57:18 -0500

    Deprecating the use_pipe() function. Fixes #2124

--- R/pipe.R
+++ R/pipe.R
@@ -12,13 +12,31 @@
 #' the roxygen template to import and re-export `%>%`. If `FALSE`, the necessary
 #' roxygen directive is added, if possible, or otherwise instructions are given.
 #'
+#'#' @description
+#' `r lifecycle::badge("deprecated")`
+#'
+#' The base R pipe has been available since R 4.1.0, which handles most cases
+#' the `magrittr` pipe handles -- `%>%` can usually just be replaced with `|>`.
+#' To read about the differences, read this `tidyverse`
+#' [blog post](https://www.tidyverse.org/blog/2023/04/base-vs-magrittr-pipe/)
+#' describing some special cases where using the base R pipe requires handling
+#' different from the `magrittr` pipe, including using
+#' * `x |> f(1, y = _)` instead of `x %>% f(1, y = .)`,
+#' * `x |> (\(x) x[[1]]))()` instead of `x %>% .[[1]]`,
+#' * `x |> (\(x) f(a = x, b = x))()` instead of `x %>% f(a = ., b = .)`, and
+#' * `x |> f()` instead of `x %>% f`.
+#'
 #' @export
+#'
+#' @keywords internal
 #'
 #' @examples
 #' \dontrun{
 #' use_pipe()
 #' }
 use_pipe <- function(export = TRUE) {
+  lifecycle::deprecate_warn(when = "3.2.2", what = "use_pipe()")
+
   check_is_package("use_pipe()")
   check_uses_roxygen("use_pipe()")
 
--- tests/testthat/_snaps/pipe.md
+++ tests/testthat/_snaps/pipe.md
@@ -5,3 +5,11 @@
     Output
       [1] "#' @importFrom magrittr %>%"
 
+# use_pipe() should produce a lifecycle deprecated warning
+
+    Code
+      use_pipe()
+    Condition
+      Warning:
+      `use_pipe()` was deprecated in usethis 3.2.2.
+
