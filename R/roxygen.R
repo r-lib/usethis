57f5271ce0ded948605fb2049b95218f26009cf5
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-09-09 11:43:00 -0700

    Prefer the modern `Config/roxygen2/version` field

--- R/roxygen.R
+++ R/roxygen.R
@@ -21,7 +21,11 @@ use_roxygen_md <- function(overwrite = FALSE) {
       "list(markdown = TRUE)",
       overwrite = FALSE
     )
-    proj_desc_field_update("RoxygenNote", roxy_ver, overwrite = FALSE)
+    proj_desc_field_update(
+      roxygen2_version_field(),
+      roxy_ver,
+      overwrite = FALSE
+    )
     ui_bullets(c("_" = "Run {.run devtools::document()}."))
     return(invisible())
   }
@@ -79,6 +83,20 @@ uses_roxygen_md <- function() {
 
 uses_roxygen <- function() {
   any(proj_desc()$has_fields(c("RoxygenNote", "Config/roxygen2/version")))
+}
+
+# roxygen2 version    DESCRIPTION field
+# -------------------------------------------
+# roxygen2 >= 8.0.0   Config/roxygen2/version
+# earlier roxygen2    RoxygenNote
+roxygen2_version_field <- function() {
+  if (
+    is_installed("roxygen2") && utils::packageVersion("roxygen2") >= "8.0.0"
+  ) {
+    "Config/roxygen2/version"
+  } else {
+    "RoxygenNote"
+  }
 }
 
 roxygen_ns_append <- function(tag) {
