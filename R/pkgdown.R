cdeb1bc676d57e16216593108d7b97c6314eefba
Michael <hausingerm@gmail.com>
2026-09-17 14:28:48 -0400

    Removed some comments

--- R/pkgdown.R
+++ R/pkgdown.R
@@ -31,8 +31,6 @@ use_pkgdown <- function(config_file = "_pkgdown.yml", destdir = "docs") {
   check_is_package("use_pkgdown()")
   check_installed("pkgdown")
 
-  # New stuff here
-  # Per issue 2065, running use_pkgdown_github_pages in a non-root branch would create an empty site.
   cfg <- github_remote_config(github_get = NA)
   check_for_bad_config(cfg)
   tr <- target_repo(cfg, ask = FALSE)
@@ -47,8 +45,6 @@ use_pkgdown <- function(config_file = "_pkgdown.yml", destdir = "docs") {
     "Running use_pkgdown functions when in a non-default branch may create an empty site. Are you sure you want to continue?",
     default_branch = default_branch
   )
-
-  # End of new stuff
 
   use_build_ignore(c(config_file, destdir, "pkgdown"))
   use_git_ignore(destdir)
